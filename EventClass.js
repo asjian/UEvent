9a13369b1ea715bf7d40495cd1295060f666157a
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-05-16 16:57:32 -0400

    Add files via upload

---
+++ EventClass.js
@@ -0,0 +1,23 @@
+class Event {
+    constructor() {
+        this.EventName = // request from server
+        this.Location = // request from server
+        this.NameOfOrganizer = // request from server
+        this.PrivacyStatus = // request from server
+        this.InPersonOrVirtual = // request from server
+        this.EventType = // request
+        this.EventTags = // request - will be a list
+        this.StartDay = // request
+        this.EndDay = // request
+        this.StartTime = // request
+        this.EndTime = // request
+        this.Description = // request
+        this.OrganizerEmail = // request
+        this.OrganizePhone = // request
+        this.OrganizerWebsite = // request
+        this.EventImage = // request
+        this.OrganizeAvatar = // request
+        this.ListOfAttendees = // request - will be a list
+        this.ListOfFollowers = // request - will be a list
+    }
+}

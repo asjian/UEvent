713a5cb006fed3f4594790a02d4b2422d3d8f951
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-06-02 23:15:55 -0400

    Add files via upload

---
+++ Components/CloseButton.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { SafeAreaView, View, Text, Settings, TouchableOpacity, StyleSheet, Image, Modal, Button, setItems } from 'react-native';
+
+// Close button used within a modal - change onPress functionality to fit your needs
+
+<Button title="X" onPress={() => setModalVisible(!modalVisible)} />

ce0b73a3d86bc7eb01015a1aa488be02097ad2ff
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-06-05 23:06:54 +0800

    Update ProfileScreen.js

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView, SafeAreaView, Image } from 'react-native';
-import ProfileButton from './components/ProfileButton';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import MyUpcomingScreen from './MyUpcomingScreen';
+import EventsFollowingScreen from './EventsFollowingScreen';
+import EventUpdatesScreen from './EventUpdatesScreen';
+import AddFriendsScreen from './AddFriendsScreen';
+import FriendRequestsScreen from './FriendRequestsScreen';
+import FriendsListScreen from './FriendsListScreen';
+import WhereFriendsScreen from './WhereFriendsScreen';
+import IncomingInvScreen from './IncomingInvScreen';
+import OutgoingInvScreen from './OutgoingInvScreen';
+import ProfileButton from '../objects/profileButton';
+//import GlobalVariables from '../../GlobalStyles';
 
 const HORIZONTALMARGIN = 20.4;
 
-export default function ProfileScreen({navigation}) {
+function MainScreen({navigation}) {
     const user = {
         username: 'Alex Jian',
         useremail: 'asjian@umich.edu',
-        avatarsource: require('./assets/avatar.png'),
+        avatarsource: require('../assets/avatar.jpeg'),
     }
 
     return (
@@ -24,17 +34,17 @@ export default function ProfileScreen({navigation}) {
             </View>
             <View style={styles.buttonContainer}>
                 <Text style = {styles.headerText}>My Event Info</Text>
-                <ProfileButton onPress = {() => {}} title = 'My Upcoming Events' />
-                <ProfileButton onPress = {() => {}} title = "Events I'm Following" />
-                <ProfileButton onPress = {() => {}} title = 'Event Updates' />
+                <ProfileButton onPress = {() => navigation.navigate('MyUpcomingScreen')} title = 'My Upcoming Events' />
+                <ProfileButton onPress = {() => navigation.navigate('EventsFollowingScreen')} title = "Events I'm Following" />
+                <ProfileButton onPress = {() => navigation.navigate('EventUpdatesScreen')} title = 'Event Updates' />
                 <Text style = {styles.headerText}>Friends</Text>
-                <ProfileButton onPress = {() => {}} title = 'Add Friends' />
-                <ProfileButton onPress = {() => {}} title = 'Friend Requests' />
-                <ProfileButton onPress = {() => {}} title = 'Friends List' />
-                <ProfileButton onPress = {() => {}} title = 'Where my Friends are Going' />
+                <ProfileButton onPress = {() => navigation.navigate('AddFriendsScreen')} title = 'Add Friends' />
+                <ProfileButton onPress = {() => navigation.navigate('FriendRequestsScreen')} title = 'Friend Requests' />
+                <ProfileButton onPress = {() => navigation.navigate('FriendsListScreen')} title = 'Friends List' />
+                <ProfileButton onPress = {() => navigation.navigate('WhereFriendsScreen')} title = 'Where my Friends are Going' />
                 <Text style = {styles.headerText}>Invitations</Text>
-                <ProfileButton onPress = {() => {}} title = 'Incoming Invitations' />
-                <ProfileButton onPress = {() => {}} title = 'Outgoing Invitations' />
+                <ProfileButton onPress = {() => navigation.navigate('IncomingInvScreen')} title = 'Incoming Invitations' />
+                <ProfileButton onPress = {() => navigation.navigate('OutgoingInvScreen')} title = 'Outgoing Invitations' />
                 <Text style = {styles.headerText}>Events I'm Hosting</Text>
                 <ProfileButton onPress = {() => {navigation.navigate('HostScreen',user)}} title = 'Manage My Events' />
             </View>
@@ -43,6 +53,32 @@ export default function ProfileScreen({navigation}) {
     </View>
     );
 }
+
+const ProfileNavigator = createSwitchNavigator(
+    {
+        MainScreen,
+        MyUpcomingScreen,
+        EventsFollowingScreen,
+        EventUpdatesScreen,
+        AddFriendsScreen,
+        FriendRequestsScreen,
+        FriendsListScreen,
+        WhereFriendsScreen,
+        IncomingInvScreen,
+        OutgoingInvScreen
+    },
+    {initialRouteName: 'MainScreen'
+    },
+)
+
+const ProfileContainer = createAppContainer(ProfileNavigator);
+
+export default function ProfileScreen({navigation}){
+    return (
+        <ProfileContainer/>
+    );
+}
+
 const styles = StyleSheet.create({
     screenContainer: {
         flex:1,
@@ -55,6 +91,7 @@ const styles = StyleSheet.create({
     },
     scrollContainer: {
         alignItems:'flex-start',
+        paddingBottom: 90
     },
     buttonContainer: {
         flex: 1,

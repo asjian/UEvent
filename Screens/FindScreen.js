a13c39c71d468342ee5bc77fea758ee9e10313c6
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-06-10 00:23:50 -0400

    Add files via upload

--- Screens/FindScreen.js
+++ Screens/FindScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {Dimensions, StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import {Dimensions, StyleSheet, Text, View, TouchableOpacity, Button, ScrollView, Image} from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import BottomSheet from 'reanimated-bottom-sheet';
 import Animated from 'react-native-reanimated';
+import TopBar from '../objects/topBar';
+import MapView from 'react-native-maps';
+import Marker from 'react-native-maps';
+import LocationPin from '../objects/locationPin';
 
 //a sub branch of the main find screen
 function DetailsScreen({ navigation }) {
@@ -21,9 +25,32 @@ function MainScreen({navigation}) {
     fall = new Animated.Value(1);
     renderInner = () => (
         <View style={styles.panel}>
-            <TouchableOpacity onPress={() => navigation.navigate('DetailsScreen')}>
-                <Text>VIEW DETAILS</Text>
-            </TouchableOpacity>
+            <View>
+                <Text>
+                    THE TITLE GOES HERE:
+                </Text>
+                <Text>
+                    lorem ipsum dolor sit amet
+                    consectetur adipiscing elit
+                    Morbi hendrerit ut felis
+                </Text>
+            </View>
+            <ScrollView>
+                <Text>
+                    THE BODY GOES HERE:
+                </Text>
+                <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit ut felis a gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt lorem sit amet ex pretium, eget placerat ante gravida. Duis egestas tellus libero, fringilla consequat nunc porta sed. Morbi pulvinar ligula arcu, in ultrices lorem semper quis. Maecenas non turpis sed felis feugiat scelerisque sed vel sapien. Aliquam dolor nisl, ullamcorper at felis at, venenatis viverra ipsum. Maecenas non ipsum massa. Duis facilisis at enim vitae porttitor. Vivamus eget porttitor mauris. Curabitur nulla velit, vehicula semper lobortis eleifend, interdum id felis.
+
+Ut ornare est lorem, sit amet interdum nulla laoreet vel. Ut ut varius sapien, sit amet pharetra nibh. Vivamus nec arcu mattis, pretium neque et, vestibulum neque. Aenean fringilla laoreet maximus. Quisque in gravida sem, a ornare justo. Proin malesuada ligula dolor. In hac habitasse platea dictumst. Sed at auctor sem. Nam blandit orci libero, id tempor mi vestibulum et. In ultricies sagittis porta. Suspendisse vel ligula id sapien convallis varius. Morbi sem leo, accumsan a placerat sed, tempor in sapien. Etiam pharetra dui justo. Nullam vitae nunc mattis, suscipit urna et, fringilla justo. Nullam et metus lacus. Proin interdum vehicula tortor, eu pharetra dolor pharetra sit amet.
+
+Proin vel neque sed quam tincidunt fringilla. Sed in aliquet justo. Sed nec ex quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer odio turpis, gravida ac velit id, semper imperdiet ex. In sagittis at nulla at tempus. Morbi viverra lectus justo, eu ultricies leo euismod sed. Morbi non lectus eu purus iaculis tempor ac eget lorem. Curabitur dictum laoreet sapien, convallis consequat nibh volutpat ac. Duis tincidunt tortor tellus. Vivamus scelerisque purus est, sagittis egestas arcu tempor eget. Duis sed congue velit, aliquam vulputate nisi.
+
+Quisque pharetra justo libero, id luctus lacus imperdiet et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean laoreet dictum orci, id volutpat urna varius a. Suspendisse orci mauris, dapibus vitae ultricies ut, tincidunt ac velit. Nulla eleifend ullamcorper sapien, ac pharetra justo placerat at. Quisque eu quam sagittis, hendrerit eros in, facilisis nunc. Suspendisse lacus massa, sodales a justo in, scelerisque mollis libero.
+
+Nam in arcu porta, volutpat neque et, finibus ligula. Donec suscipit placerat interdum. Fusce magna urna, iaculis ac tincidunt quis, malesuada ut leo. Mauris sed magna a erat laoreet dictum. Morbi viverra, enim non accumsan placerat, felis diam efficitur erat, nec pharetra quam orci imperdiet elit. Praesent accumsan congue tellus quis convallis. Donec in mi et enim dignissim consequat. Pellentesque venenatis, nunc at malesuada sollicitudin, elit eros aliquam elit, vel malesuada dolor nisi sed lacus. Curabitur egestas nisi tellus, nec venenatis urna tristique lacinia. Suspendisse consequat, sapien eu gravida posuere, ex libero condimentum enim, id pretium leo libero ut sapien. Sed varius eros quis velit gravida, ut venenatis magna suscipit. Nam fermentum erat vel lectus ornare suscipit. Pellentesque volutpat placerat tellus nec pulvinar. Aliquam consequat, nibh vel varius sollicitudin, elit metus pulvinar velit, vitae ultrices est lectus id erat.
+                </Text>
+            </ScrollView>
         </View>
     );
     
@@ -37,24 +64,59 @@ function MainScreen({navigation}) {
     );
 
     return (
-        <View style={styles.container}>
-            <BottomSheet
-                ref={this.bs}
-                snapPoints={[windowHeight, 300, 0]}
-                renderContent={this.renderInner}
-                renderHeader={this.renderHeader}
-                initialSnap={1}
-                callbackNode={this.fall}
-                enabledGestureInteraction={true}
+        <MapView style={styles.map}
+
+            initialRegion = {{
+            latitude: 42.278,
+            longitude: -83.738,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.05,
+            }
+            }
+            
+        >
+            <View style={styles.topbar}>
+                    <TopBar/>
+            </View>
+            <MapView.Marker
+                coordinate = {{latitude: 42.278, longitude: -83.738}}
+                title = 'Fiji Tailgate'
             />
-            <Animated.View style={{margin: 20,
-                opacity: Animated.add(0.1, Animated.multiply(this.fall, 1.0)),
-            }}>
-            <TouchableOpacity onPress={()=>bs.current.snapTo(1)}>
-                <Text>tap here</Text>
-            </TouchableOpacity>
-            </Animated.View>   
-        </View>
+            <MapView.Marker
+                coordinate = {{latitude: 42.27, longitude: -83.74}}
+                onPress={()=>bs.current.snapTo(1)}
+            >
+                <Image source={require('../assets/avatar.jpeg')} style={{width: 20, height: 20}}  />
+                <Text>John wkaing up</Text>
+            </MapView.Marker>
+              {/*  <View style={styles.container}>
+                <View style={styles.topbar}>
+                    <TopBar/>
+        </View> 
+                <BottomSheet
+                    ref={this.bs}
+                    snapPoints={[windowHeight, 300, 0]}
+                    renderContent={this.renderInner}
+                    renderHeader={this.renderHeader}
+                    initialSnap={2}
+                    callbackNode={this.fall}
+                    enabledGestureInteraction={true}
+                />
+                <Animated.View style={{margin: 20,
+                    opacity: Animated.add(0.1, Animated.multiply(this.fall, 1.0)),
+                }}>
+                
+                <TouchableOpacity onPress={()=>bs.current.snapTo(1)}>
+                    <Text>tap here</Text>
+                </TouchableOpacity>
+                
+                </Animated.View>   
+            
+            </View>
+            */}
+        
+        </MapView>
+        
     );
 }
 
@@ -76,10 +138,15 @@ export default function FindScreen() {
 }
 
 const styles = StyleSheet.create({
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    },
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+    },
+    topbar: {
+        margin: 10
     },
     header: {
         backgroundColor: '#fff',
@@ -105,6 +172,6 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: '#fff',
         paddingTop: 20,
-        paddingBottom: Dimensions.get('window').height,
+        //paddingBottom: Dimensions.get('window').height,
     }
 })

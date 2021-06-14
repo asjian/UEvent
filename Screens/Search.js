5f87e16637e4e3d4278a45745c648cf30f6867b9
asjian <79596814+asjian@users.noreply.github.com>
2021-06-13 21:36:28 -0400

    Update Search.js

--- Screens/Search.js
+++ Screens/Search.js
@@ -3,50 +3,19 @@ import React, {useState,Component} from 'react';
 import {StyleSheet, Text, View,SafeAreaView,Keyboard,TouchableWithoutFeedback,TouchableOpacity,ScrollView} from 'react-native';
 import {AntDesign} from '@expo/vector-icons';
 import { SearchBar } from 'react-native-elements';
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
-import { ListItem } from 'react-native-elements/dist/list/ListItem';
+import CategoryList from './CategoryList';
 
-function CategoryList({navigation}) {
-    console.log('category list');
-    const categories = [{name:'Student Orgs',key:0,},{name:'Parties',key:1,},{name:'Sports',key:2,},{name:'Art/Design',key:3,}];
-
-    return (
-        <View style = {categoryStyles.container}>
-            <ScrollView>
-                {categories.map((item) => {
-                    return (
-                        <View key = {item.key}>
-                            <TouchableOpacity>
-                                <Text>item.name</Text>
-                            </TouchableOpacity>
-                        </View>
-                    )
-                })}
-            </ScrollView>
-            <TouchableOpacity onPress = {navigation.navigate('SearchScreen')}>
-                <Text>Select</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
-const categoryStyles = StyleSheet.create({
-    container: {
-        height: '80%',
-        marginTop: 'auto',
-        marginBottom: 50,
-        backgroundColor: '#fffbf2',
-    },
-})
-function SearchScreen({navigation,toggleVisibility}) {
+export default function Search({navigation}) {
     console.log('reached Search Screen');
     const [searchText, setSearchText] = useState('');
 
     return (
         <TouchableWithoutFeedback onPress = {() => Keyboard.dismiss()}>
-        <View style = {styles.container}>
+        <View style = {styles.outerContainer}>
+        <View style = {styles.innerContainer}>
                 <Text style = {styles.headerText}>Search</Text>
                 <View style = {styles.close}>
-                    <AntDesign name = 'closecircleo' size = {30} onPress = {() => toggleVisibility(false)}/>
+                    <AntDesign name = 'closecircleo' size = {30} onPress = {() => navigation.goBack()}/>
                 </View>
                 <SearchBar placeholder = 'Event Names' 
                 onChangeText = {(search) => {setSearchText(search); console.log(searchText)}}
@@ -55,39 +24,24 @@ function SearchScreen({navigation,toggleVisibility}) {
                 containerStyle = {styles.searchContainer}/>
                 <View>
                     <Text style = {styles.headerText}>Filters</Text>
-                    <TouchableOpacity onPress = {navigation.navigate('CategoryList')}> 
+                    <TouchableOpacity onPress = {() => navigation.push('CategoryList')}> 
                         <Text style = {styles.headerText}>Event Categories</Text>
                     </TouchableOpacity>
                 </View>
+        </View>
         </View>
         </TouchableWithoutFeedback>
     );
 }
 
-const SearchNavigator = createSwitchNavigator(
-    {
-    SearchScreen,CategoryList
-    },
-    {
-    initialRouteName: "SearchScreen"
-    },
-)
-
-const SearchContainer = createAppContainer(SearchNavigator);
-
-export default function Search() {
-    return (
-        <SearchContainer/>
-    );
-}
-
 
 const styles = StyleSheet.create({
-    container: {
-        height: '80%',
-        marginTop: 'auto',
-        marginBottom: 50,
+    outerContainer: {
         backgroundColor: '#fffbf2',
+        flex: 1,
+    },
+    innerContainer: {
+        marginTop: 50,
     },
     searchContainer: {
         marginTop: 10,

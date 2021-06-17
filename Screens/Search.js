b6de705225db29888b653e20ad201e5e7fcc1596
asjian <79596814+asjian@users.noreply.github.com>
2021-06-16 22:06:04 -0400

    Update Search.js

--- Screens/Search.js
+++ Screens/Search.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState,Component} from 'react';
-import {StyleSheet, Text, View,SafeAreaView,Keyboard,TouchableWithoutFeedback,TouchableOpacity,ScrollView,Image} from 'react-native';
+import React, {useEffect,useState,Component} from 'react';
+import {StyleSheet, Text, View,SafeAreaView,Keyboard,TouchableWithoutFeedback,TouchableOpacity,ScrollView,Image,Alert} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import { SearchBar } from 'react-native-elements';
 import CategoryList from './CategoryList';
 import {AntDesign} from '@expo/vector-icons';
 
-export default function Search({navigation}) {
 
+export default function Search({navigation}) {
+    const [categoryLabel, setCategoryLabel] = useState('Event Categories');
+    /*navigation.addListener('didFocus', () => {
+        console.log('focused 2');
+        setCategoryLabel('Selection');
+    });*/
+    console.log('reached Search');
+    const buttonTextDecider = (typeName) => {
+        if(typeName == 'Categories') {
+            if(navigation.getParam('Categories').length == 0)
+                return <Text style = {styles.buttonText}>Event Categories</Text>;     
+        }
+        return <Text style = {styles.buttonText}>Whoops</Text>;   
+    }
+    const filterHandler = (screenName) => {
+        navigation.navigate(screenName);
+    }
     const searchSubmitHandler = (searchText) => {
         console.log(searchText);
     }
@@ -40,19 +56,19 @@ export default function Search({navigation}) {
                 <View>
                     <Text style = {[styles.headerText,{marginTop: 62, marginBottom: 22}]}>Filters</Text>
 
-                    <TouchableOpacity onPress = {() => navigation.push('CategoryList')} style = {styles.buttonStyle}>
+                    <TouchableOpacity onPress = {() => filterHandler('CategoryList')} style = {styles.buttonStyle}>
                         <Image source = {require('../assets/categories.png')} size = {30} style = {styles.leftIcon}/> 
-                        <Text style = {styles.buttonText}>Event Categories</Text>
+                        {buttonTextDecider('Categories')}
                         <AntDesign name = 'right' size = {30} color = '#828181' style = {styles.rightIcon}/>
                     </TouchableOpacity>
                 
-                    <TouchableOpacity onPress = {() => navigation.push('CategoryList')} style = {styles.buttonStyle}>
+                    <TouchableOpacity onPress = {() => filterHandler('CategoryList')} style = {styles.buttonStyle}>
                         <Image source = {require('../assets/clock.png')} size = {30} style = {styles.leftIcon}/> 
                         <Text style = {[styles.buttonText, {marginLeft: 18}]}>Time Range</Text>
                         <AntDesign name = 'right' size = {30} color = '#828181' style = {[styles.rightIcon,{marginLeft: 148}]}/>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress = {() => navigation.push('CategoryList')} style = {styles.buttonStyle}>
+                    <TouchableOpacity onPress = {() => filterHandler('CategoryList')} style = {styles.buttonStyle}>
                         <Image source = {require('../assets/filter.png')} size = {30} style = {styles.leftIcon}/> 
                         <Text style = {[styles.buttonText,{marginLeft: 17}]}>Other Filters</Text>
                         <AntDesign name = 'right' size = {30} color = '#828181' style = {[styles.rightIcon, {marginLeft:140}]}/>

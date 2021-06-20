8499a39b68fb0c59e88a03391b50f70d24848527
asjian <79596814+asjian@users.noreply.github.com>
2021-06-19 20:32:08 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import NavBar from './app/routes/navBar';
+import AppContext from './app/objects/AppContext';
 
 const App = () => {
+  const [showNavBar,setShowNavBar] = useState(true);
+  const toggleShowNavBar = () => {
+    setShowNavBar(!showNavBar);
+  }
+  const globals = {
+    navBarVisible: showNavBar,
+    toggleShowNavBar,
+  }
   return (
     //if the user is on the app the first time
     //<WelcomeNavigator />
-    
-    <NavigationContainer>
-      <NavBar/>
-    </NavigationContainer>
-    
+    <AppContext.Provider value = {globals}>
+      <NavigationContainer>
+        <NavBar/>
+      </NavigationContainer>
+    </AppContext.Provider>
     //<WelcomeScreen/>
   );
 }
 
 export default App;
-

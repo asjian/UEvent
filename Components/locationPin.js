f08a40d97a2de37f3728f1d9569188792055871a
asjian <79596814+asjian@users.noreply.github.com>
2021-07-08 13:54:30 -0400

    Update locationPin.js

--- Components/locationPin.js
+++ Components/locationPin.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { TouchableOpacity, Image, StyleSheet, Text} from 'react-native';
+import { View, Image, StyleSheet, Text} from 'react-native';
 
 
 {/* Map marker icon */}
-export default function LocationPin({title,onPress}) {
+export default function LocationPin({title}) {
     return (
-        <TouchableOpacity onPress = {onPress}>
+        <View>
             <Image style={styles.MapMarkerStyle} source={require('../assets/MapMarker.png')} />
             <Text>{title}</Text>
-        </TouchableOpacity>
+        </View>
     );
 }
     
-    
-    
-
 const styles = StyleSheet.create({
     MapMarkerStyle : {
         height: 40,

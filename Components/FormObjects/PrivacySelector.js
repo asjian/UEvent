0ba4e6e7912243ec9ca7959c0041adb3e7a8f20a
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-09 18:35:32 -0400

    Update PrivacySelector.js

--- Components/FormObjects/PrivacySelector.js
+++ Components/FormObjects/PrivacySelector.js
@@ -1,19 +1,13 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import ModalSelector from 'react-native-modal-selector';
 import { SafeAreaView, View, Text, Button, StyleSheet, TextInput, Image, ScrollView, Alert } from 'react-native';
 
 
-class PrivacySelector extends Component {
+const PrivacySelector = ({onChange, value}) => {
 
-    constructor(props) {
-        super(props);
+        const [textInputValue, setTextInputValue] = useState('');
 
-        this.state = {
-            textInputValue: ''
-        }
-    }
-
-    render() {
+    
         let index = 0;
         const data = [
             { key: index++, section: true, label: 'Privacy' },
@@ -22,6 +16,12 @@ class PrivacySelector extends Component {
             // etc...
             // Can also add additional custom keys which are passed to the onChange callback
         ];
+
+        const handleInput = (option)=> { 
+            setTextInputValue(option.label);
+            onChange('Privacy', option.label);
+            
+        };
 
         return (
             <View style={{flex:1, justifyContent:'space-around', padding:10}}>
@@ -32,19 +32,20 @@ class PrivacySelector extends Component {
                     accessible={true}
                     scrollViewAccessibilityLabel={'Scrollable options'}
                     cancelButtonAccessibilityLabel={'Cancel Button'}
-                    onChange={(option)=>{ this.setState({textInputValue:option.label})}}>
+                    onChange={handleInput}>
 
                     <TextInput
                         style={{borderWidth:1, borderColor:'#ccc', padding:10, height:40}}
                         editable={false}
                         placeholder="Public/Private"
-                        value={this.state.textInputValue} />
+                        value={value} 
+                        
+                    />
 
                 </ModalSelector>
             </View>
         );
     }
-}
 
 // export
 export default PrivacySelector;

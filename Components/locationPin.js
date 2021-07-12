af058d51bfdb72b54175c7da5e24caa56ac72d59
asjian <79596814+asjian@users.noreply.github.com>
2021-07-12 19:49:36 -0400

    Update locationPin.js

--- Components/locationPin.js
+++ Components/locationPin.js
@@ -4,18 +4,68 @@ import { View, Image, StyleSheet, Text} from 'react-native';
 
 {/* Map marker icon */}
 export default function LocationPin({title}) {
+    let displayTitle = '';
+
+    let words = title.split(" ");
+    let runningLength = 0;
+
+    for(let i=0;i<words.length;i++) {
+        displayTitle += words[i];
+        runningLength += words[i].length;
+        if(runningLength > 12) {
+            displayTitle += '\n';
+            runningLength = 0;
+        }
+        else {
+            displayTitle += ' ';
+        }
+    }
+    displayTitle = displayTitle.slice(0, -1); 
+
     return (
-        <View>
-            <Image style={styles.MapMarkerStyle} source={require('../assets/MapMarker.png')} />
-            <Text>{title}</Text>
+        <View style = {styles.container}>
+            <View style = {styles.textContainer}>
+                <Text style = {styles.titleText}>{displayTitle}</Text>
+            </View>
+            <View style = {styles.triangle}>
+            </View>
+            <Image style={styles.mapMarkerStyle} source={require('../assets/MapMarker.png')} />
         </View>
     );
 }
     
 const styles = StyleSheet.create({
-    MapMarkerStyle : {
-        height: 40,
-        width: 40,
-    }
+    container: {
+        alignItems: 'center',
+    },
+    mapMarkerStyle : {
+        height: 50,
+        width: 50,
+    },
+    titleText: {
+        fontSize: 11,
+        fontWeight: '500',
+        paddingHorizontal: 4,
+        paddingVertical: 2,
+    },
+    textContainer: {
+        backgroundColor: 'white',
+        borderRadius: 5,
+    },
+    triangle: {
+        width: 0,
+        height: 0,
+        backgroundColor: 'transparent',
+        borderStyle: 'solid',
+        borderTopWidth: 7,
+        borderRightWidth: 7,
+        borderBottomWidth: 0,
+        borderLeftWidth: 7,
+        borderTopColor: "white",
+        borderRightColor: 'transparent',
+        borderBottomColor: 'transparent',
+        borderLeftColor: 'transparent',
+        marginBottom: -7,
+    },
 })
 

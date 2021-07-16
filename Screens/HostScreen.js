0f187adee40544c37e4d9454936faeb6e10896fa
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-15 21:59:57 -0400

    Update HostScreen.js

--- Screens/HostScreen.js
+++ Screens/HostScreen.js
@@ -6,14 +6,25 @@ import CreateNewEventScreen from './CreateNewEvent';
 
 const Stack = createStackNavigator()
 
-
-
-
 function HostScreen(props) {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Events I'm Hosting" component={HostNavBar} />
-            <Stack.Screen name="Create a New Event" component={CreateNewEventScreen} options={{headerShown: false}}/>
+            <Stack.Screen
+                name="Events I'm Hosting"
+                component={HostNavBar}
+                options={{
+                    headerStyle: {
+                        height: 125,
+                        backgroundColor: '#FFFBF3'
+                    },
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                        fontSize: 24,
+
+                    }
+                }}
+            />
+            <Stack.Screen name="Create a New Event" component={CreateNewEventScreen} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }

19feb7f60fb895ccb4457e4854c05f26c383ffc1
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-15 22:01:43 -0400

    Update HostNavBar.js

--- Components/HostNavBar.js
+++ Components/HostNavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
+import { StyleSheet } from 'react-native';
 import UpcomingEventsScreen from '../screens/UpcomingEvents';
 import PastEventsScreen from '../screens/PastEvents';
 
@@ -8,7 +8,24 @@ const HostTabNav = createMaterialTopTabNavigator();
 
 const HostNavBar = () => {
     return (
-        <HostTabNav.Navigator>
+        <HostTabNav.Navigator
+            tabBarOptions={{
+                indicatorStyle: {
+                    borderWidth: 1,
+                    borderColor: '#FAB400'
+                },
+                activeTintColor: '#FAB400',
+                inactiveTintColor: 'black',
+                labelStyle: {
+                    fontWeight: '500',
+                    fontSize: 18,
+                    textTransform: 'none'
+                },
+                style: {
+                    backgroundColor: '#FFFBF3'
+                }
+            }}
+        >
             <HostTabNav.Screen name="Upcoming Events" component={UpcomingEventsScreen} />
             <HostTabNav.Screen name="Past Events" component={PastEventsScreen} />
         </HostTabNav.Navigator>

55c09a29ec2337bb8cbf2057230e2385b6fc9d16
asjian <79596814+asjian@users.noreply.github.com>
2021-07-19 14:30:13 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,13 +5,29 @@ import AppContext from './app/objects/AppContext';
 import InitialNav from './app/routes/initialNav';
 
 const App = () => {
+  
   const [showNavBar,setShowNavBar] = useState(true);
   const toggleShowNavBar = (value) => {
     setShowNavBar(value);
   }
+  const [user,setUser] = useState({
+    id: -1,
+    Name: '',
+    Email: 'nan@umich.edu',
+    Organization: "No",
+    Admin: "No",
+    UpcomingEvents: '',
+    EventsHosting: '',
+  });
+  const initializeUser = (value) => {
+    setUser(value);
+  }
   const globals = {
     navBarVisible: showNavBar,
     toggleShowNavBar,
+
+    user: user,
+    initializeUser,
   }
   return (
     //<WelcomeNavigator />
@@ -23,5 +39,4 @@ const App = () => {
     //<WelcomeScreen/>
   );
 }
-
 export default App;

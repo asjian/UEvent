de681d4d585d95ea187c43c9a0df9ee045a28d6b
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-19 21:00:22 -0400

    Update PrivacySelector.js

--- Components/FormObjects/PrivacySelector.js
+++ Components/FormObjects/PrivacySelector.js
@@ -1,51 +1,56 @@
+import {Picker} from '@react-native-picker/picker';
 import React, {useState} from 'react';
-import ModalSelector from 'react-native-modal-selector';
-import { SafeAreaView, View, Text, Button, StyleSheet, TextInput, Image, ScrollView, Alert } from 'react-native';
-
+import RNPickerSelect from 'react-native-picker-select';
+import { StyleSheet } from 'react-native';
 
 const PrivacySelector = ({onChange, value}) => {
+    return (
+        <RNPickerSelect
+            onValueChange={(value) => onChange('Privacy', value)}
+            value={value}
 
-        const [textInputValue, setTextInputValue] = useState('');
+            items={[
+                { label: 'Public', value: 'Public' },
+                { label: 'Private', value: 'Private' },
+            ]}
+            style={pickerSelectStyles}
+            placeholder={{
+                label: 'Public/Private',
+                value: null,
+                
+            }}
+        />
+    );
+};
 
-    
-        let index = 0;
-        const data = [
-            { key: index++, section: true, label: 'Privacy' },
-            { key: index++, label: 'Public' },
-            { key: index++, label: 'Private' },
-            // etc...
-            // Can also add additional custom keys which are passed to the onChange callback
-        ];
-
-        const handleInput = (option)=> { 
-            setTextInputValue(option.label);
-            onChange('Privacy', option.label);
-            
-        };
-
-        return (
-            <View style={{flex:1, justifyContent:'space-around', padding:10}}>
-
-                <ModalSelector
-                    data={data}
-                    supportedOrientations={['portrait']}
-                    accessible={true}
-                    scrollViewAccessibilityLabel={'Scrollable options'}
-                    cancelButtonAccessibilityLabel={'Cancel Button'}
-                    onChange={handleInput}>
-
-                    <TextInput
-                        style={{borderWidth:1, borderColor:'#ccc', padding:10, height:40}}
-                        editable={false}
-                        placeholder="Public/Private"
-                        value={value} 
-                        
-                    />
-
-                </ModalSelector>
-            </View>
-        );
-    }
-
-// export
 export default PrivacySelector;
+
+const styles = StyleSheet.create({
+    InputBox: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    }
+})
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    },
+    inputAndroid: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    },
+  });

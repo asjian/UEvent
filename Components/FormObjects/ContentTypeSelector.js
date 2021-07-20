a04739957c7214aab2930845b7fff7fc1fd41c00
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-19 20:57:08 -0400

    Update ContentTypeSelector.js

--- Components/FormObjects/ContentTypeSelector.js
+++ Components/FormObjects/ContentTypeSelector.js
@@ -1,69 +1,62 @@
+import {Picker} from '@react-native-picker/picker';
 import React, {useState} from 'react';
-import ModalSelector from 'react-native-modal-selector-searchable';
-import { SafeAreaView, View, Text, Button, StyleSheet, TextInput, Image, ScrollView, Alert } from 'react-native';
-
+import RNPickerSelect from 'react-native-picker-select';
+import { StyleSheet } from 'react-native';
 
 const ContentTypeSelector = ({onChange, value}) => {
+    return (
+        <RNPickerSelect
+            onValueChange={(value) => onChange('ContentType', value)}
+            value={value}
 
-    const [textInputValue, setTextInputValue] = useState('');
+            items={[
+                { label: 'Software', value: 'Software' },
+                { label: 'Research', value: 'Research' },
+                { label: 'Greek Life', value: 'Greek Life' },
+                { label: 'Professional', value: 'Professional' },
+                { label: 'Consulting', value: 'Consulting' },
+                { label: 'Blockchain', value: 'Blockchain' },
+                { label: 'Finance', value: 'Finance' },
+                { label: 'UX Design', value: 'UX Design' }
+            ]}
+            style={pickerSelectStyles}
+            placeholder={{
+                label: 'Egs. UX Design, House Party, etc.',
+                value: null,
+                
+            }}
+        />
+    );
+};
 
-        let index = 0;
-        const data = [
-            { key: index++, section: true, label: 'Content Type' },
-            { key: index++, label: 'House Party' },
-            { key: index++, label: 'UX Design' },
-            { key: index++, label: 'Engieering'},
-            { key: index++, label: 'Consulting'},
-            { key: index++, label: 'Blockchain'},
-            { key: index++, label: 'Research'},
-            { key: index++, label: 'Software'},
-            { key: index++, label: 's'},
-            { key: index++, label: 'f'},
-            { key: index++, label: 'a'},
-            { key: index++, label: 'i'},
-            { key: index++, label: 'p'},
-            { key: index++, label: '00'},
-            { key: index++, label: '01'},
-            { key: index++, label: '02'},
-            { key: index++, label: '03'},
-            { key: index++, label: '04'},
-            { key: index++, label: '05'},
-            { key: index++, label: '06'},
-            { key: index++, label: '07'},
-            { key: index++, label: '08'},
-            { key: index++, label: '09'}
-            // etc...
-            // Can also add additional custom keys which are passed to the onChange callback
-            
-        ];
-
-        const handleInput = (option) => {
-            setTextInputValue(option.label);
-            onChange('ContentType', option.label);
-        }
-
-        return (
-            <View style={{flex:1, justifyContent:'space-around', padding:10}}>
-
-                <ModalSelector
-                    data={data}
-                    supportedOrientations={['portrait']}
-                    accessible={true}
-                    scrollViewAccessibilityLabel={'Scrollable options'}
-                    cancelButtonAccessibilityLabel={'Cancel Button'}
-                    onChange={handleInput}>
-
-                    <TextInput
-                        style={{borderWidth:1, borderColor:'#ccc', padding:10, height:40}}
-                        editable={false}
-                        placeholder="Egs. UX Design, House Party, etc."
-                        value={value} />
-
-                </ModalSelector>
-            </View>
-        );
-    }
-
-
-// export
 export default ContentTypeSelector;
+
+const styles = StyleSheet.create({
+    InputBox: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    }
+})
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    },
+    inputAndroid: {
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderColor: '#C4C4C4',
+        padding: 8,
+        width: '80%',
+        margin: 10
+    },
+  });

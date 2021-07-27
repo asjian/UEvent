ee0427a0a9aed2bb5d26d28ddbe7adf7f784f286
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-26 20:54:47 -0400

    Update StartTimeSelector.js

--- Components/FormObjects/StartTimeSelector.js
+++ Components/FormObjects/StartTimeSelector.js
@@ -1,35 +1,82 @@
-import React, { Component } from 'react'
-import DatePicker from 'react-native-datepicker'
+import React, {useState} from 'react';
+import {View, Button, Platform, StyleSheet, TouchableOpacity, Text, Modal} from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-export default class StartTimeSelector extends Component {
-  constructor(props){
-    super(props)
-    this.state = {date:""}
-  }
+export const StartTimeSelector = ({onChangeFormik, realStartDate, realEndDate}) => {
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('time'); 
+  const [show, setShow] = useState(false);
 
-  render(){
-    return (
-      <DatePicker
-        style={{width: 400}}
-        date={this.props.value}
-        mode="time"
-        placeholder="select start time"
-        format="LT"
-        confirmBtnText="Confirm"
-        cancelBtnText="Cancel"
-        customStyles={{
-          
-          dateInput: {
-            marginLeft: 10
-          }
-          // ... You can check the source to find the other keys.
-        }}
-        onDateChange={(date) => {
-            this.setState({date: date})
-            this.props.onChange('StartTime', date)
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+    let styledTime = currentDate.toLocaleString('en-US', { hour: 'numeric',minute: '2-digit', hour12: true });
+    onChangeFormik('StartTime', styledTime);
+    onChangeFormik('StartTimeFormPurposes', currentDate);
+    onChangeFormik('RealStartDateTime', new Date(new Date(realStartDate).setHours(currentDate.getHours(),currentDate.getMinutes(),currentDate.getSeconds(), currentDate.getMilliseconds())));
     
-        }}
-      />
-    )
-  }
-}
+
+   
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+
+  const dateFormatter = (dateString) => {
+        return (
+        <Text>{dateString.substring(0,3)+', '+dateString.substring(4,dateString.indexOf(':')-8)} </Text>
+        );
+    }
+
+
+  return (
+    <View>
+        <View style={{marginLeft: 20, marginTop: 10}}>
+            <TouchableOpacity onPress = {showTimepicker}>
+                <Text style={styles.dateText}>{realStartDate.toLocaleString('en-US', { hour: 'numeric',minute: '2-digit', hour12: true })}</Text>
+            </TouchableOpacity>
+        </View>
+        <Modal visible = {show} transparent = {true} animationType = 'slide'>
+                <View style = {styles.dateModal}>
+                    <TouchableOpacity onPress = {()=>setShow(false)}>
+                        <View style = {{backgroundColor:'#d7d7d7',alignItems:'flex-end',width:'100%'}}>
+                            <Text style = {{fontSize:18,color:'#0085ff',paddingVertical:8,marginHorizontal:15}}>Done</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        mode = {mode}
+                        value = {realStartDate}
+                        onChange = {onChange}
+                        display='default'
+        
+                    />
+                </View>
+        </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    dateText: {
+        fontSize: 22,
+        fontWeight: '600',
+        color: '#09189E',
+    },
+    dateModal: {
+        height: '30%',
+        marginTop: 'auto',
+        backgroundColor: 'white',
+    }
+})

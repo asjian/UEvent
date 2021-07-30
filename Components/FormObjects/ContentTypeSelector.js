ba13bbaee18ccd15c78f0bff64a350a6a97f47eb
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-07-29 22:19:59 -0400

    Update ContentTypeSelector.js

--- Components/FormObjects/ContentTypeSelector.js
+++ Components/FormObjects/ContentTypeSelector.js
@@ -1,72 +1,119 @@
-import React, { Component } from "react"
-import { View, Text, StyleSheet } from "react-native"
-import Select2 from "react-native-select-two"
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 
-const mockData = [
-    { id: 'Parties', name: 'Parties' },
-    { id: 'Career', name: 'Career' },
-    { id: 'Community', name: 'Community' },
-    { id: 'Greek Life', name: 'Greek Life' },
-    { id: 'Games', name: 'Games' },
-    { id: 'Activism', name: 'Activism' },
-    { id: 'Art/Design', name: 'Art/Design' },
-    { id: 'Performance', name: 'Performance' },
-    { id: 'Exhibition', name: 'Exhibition' },
-    { id: 'Science/Tech', name: 'Science/Tech' },
-    { id: 'Language/Literature', name: 'Language/Literature' },
-    { id: 'Other', name: 'Other' }
-]
+const items = [
+  // this is the parent or 'item'
+  {
+    name: 'Categories',
+    id: 0,
+    // these are the children or 'sub items'
+    children: [
+      {
+        name: 'Parties',
+        id: 'Parties',
+      },
+      {
+        name: 'Career',
+        id: 'Career',
+      },
+      {
+        name: 'Greek Life',
+        id: 'Greek Life',
+      },
+      {
+        name: 'Activism',
+        id: 'Activism',
+      },
+      {
+        name: 'Art/Design',
+        id: 'Art/Design',
+      },
+      {
+        name: 'Performance',
+        id: 'Performance',
+      },
+      {
+        name: 'Exhibition',
+        id: 'Exhibition',
+      },
+      {
+        name: 'Science/Tech',
+        id: 'Science/Tech',
+      },
+      {
+        name: 'Language/Literature',
+        id: 'Language/Literature',
+      },
+      {
+        name: 'Other',
+        id: 'Other',
+      }
+    ],
+  },
 
-// create a component
-export class ContentTypeSelector extends Component {
-    state = {
-        data: this.props.form.values.ContentType
-    }
+
+];
+
+
+
+export class ContentTypeSelector2 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedItems: [],
+        maxItems: 3,
+        maxItem: false
+    };
+  }
+
+  
+  
+
+  onSelectedItemsChange = (selectedItems) => {
+    if (selectedItems.length >= this.state.maxItems) {
+        if (selectedItems.length === this.state.maxItems) {
+          this.setState({ selectedItems })
+          this.props.form.values.ContentType = selectedItems;
+        }
+        this.setState({
+          maxItem: true,
+        })
+        return
+      }
+      this.setState({
+        maxItem: false,
+      })
+
+    this.setState({ selectedItems });
+    this.props.form.values.ContentType = selectedItems;
+  };
+
   render() {
     return (
-      <View >
-        <Select2
-
-          isSelectSingle={false}
-          showSearchBox={false}
-          style={{ width: '88%', marginLeft: 20, marginTop: 10, marginBottom: 10}}
-          colorTheme="blue"
-          popupTitle="Select item"
-          title="Select item"
-          selectButtonText='Done'
-          cancelButtonText='Cancel'
-          data={mockData}
-          onSelect={data => {
-            this.setState({data})
-            let i;
-            
-            // clear array
-            for (i = 0; i < this.props.form.values.ContentType.length + 1; ++i) {
-                this.props.pop();
-            }
-            // set new array given by data (might be a more efficient way to do this)
-            let j;
-            for (j = 0; j < data.length; ++j) {
-                this.props.push(data[j]);
-            }
-            
-          }}
-          onRemoveItem={data => {
-            this.setState({ data })
-            let i;
-            let index;
-            // clear array
-            for (i = 0; i < this.props.form.values.ContentType.length + 1; ++i) {
-                this.props.pop();
-            }
-            // set new array given by data (might be a more efficient way to do this)
-            let j;
-            for (j = 0; j < data.length; ++j) {
-                this.props.push(data[j]);
-            }
-          }}
+      <View>
+        <SectionedMultiSelect
+          items={items}
+          IconRenderer={MaterialIcons}
+          uniqueKey="id"
+          subKey="children"
+          selectText="Choose some things..."
+          showDropDowns={false}
+          readOnlyHeadings={true}
+          onSelectedItemsChange={this.onSelectedItemsChange}
+          selectedItems={this.props.form.values.ContentType}
+          modalWithSafeAreaView={true}
+          confirmText={`${this.props.form.values.ContentType.length}/${this.state.maxItems} - ${
+            this.state.maxItems === this.props.form.values.ContentType.length  ? 'Max selected - Confirm' : 'Confirm'
+          }`}
+          styles={{subItemText: {fontSize: 23}, itemIconStyle: {fontSize: 23}, itemText: {fontSize: 23}, modalWrapper: {borderWidth: 2} }}
         />
       </View>
-    )
+    );
   }
 }
+ 
+const styles = StyleSheet.create({
+   
+})

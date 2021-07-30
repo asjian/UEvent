981c46b73232c19211084e717f6f5be5eb14222a
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-07-29 23:01:56 -0400

    Update FindScreen.js

--- Screens/FindScreen.js
+++ Screens/FindScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useContext, useEffect} from 'react';
-import {Dimensions, StyleSheet, Text, View, TouchableOpacity, Button, ScrollView, Image, TouchableWithoutFeedback} from 'react-native';
+import {Dimensions, StyleSheet, Text, View, TouchableOpacity, Button, ScrollView, Image} from 'react-native';
 import { createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import BottomSheet from 'reanimated-bottom-sheet';
@@ -10,12 +10,13 @@ import Search from './Search';
 import CategoryList from './CategoryList';
 import TimeRange from './TimeRange';
 import OtherFilters from './OtherFilters';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import Marker from 'react-native-maps';
-import Geocoder from 'react-native-geocoding';
+//import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
+import {Marker} from 'react-native-maps';
 import LocationPin from '../objects/locationPin';
 import AppContext from '../objects/AppContext';
 import Globals from '../../GlobalVariables';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 //a sub branch of the main find screen
 function DetailsScreen({ navigation }) {
@@ -30,14 +31,14 @@ function DetailsScreen({ navigation }) {
 function MainScreen({navigation}) {
     const myContext = useContext(AppContext);
     /*
-    Geocoder.init('AIzaSyBCGE9PpyEZ1FK9OBtL9uMgWW6jl8efD1I');
+    Geocoder.init('AIzaSyCwpmhlqGnuN1m-MdKp0FOpVZwFR1QFqug');
     Geocoder.from("2281 Bonisteel Blvd, Ann Arbor, MI, 48109")
 		.then(json => {
 			var location = json.results[0].geometry.location;
 			console.log(location);
 		})
 		.catch(error => console.warn(error));
-    */
+    */  
     const [currentEvent,setCurrentEvent] = useState({
       "id": 1,
       "Name": "OSU Pregame Tailgate",
@@ -62,38 +63,244 @@ function MainScreen({navigation}) {
       "InPersonVirtual": "In Person",
       "OtherCategories": "Greek Life Social Food/Drink "
     });
+
+    const categories = [{name:'Extracurriculars', icon: require('../assets/club.png'), key:0,},
+    {name:'Parties', icon: require('../assets/parties.png'),key:1,}, {name:'Social',icon: require('../assets/social.png'),key:2,},
+    {name:'Career',icon: require('../assets/career.png'),key:3,}, {name:'Networking',icon: require('../assets/networking.png'),key:4,},
+    {name:'Community',icon: require('../assets/community.png'),key:5,}, {name:'Fair/Festival',icon: require('../assets/festival.png'),key:6,}, 
+    {name:'Greek Life',icon: require('../assets/greeklife.png'),key:7,}, {name:'Sports',icon: require('../assets/sports.png'),key:8,}, 
+    {name:'Games',icon: require('../assets/games.png'),key:9,}, {name:'Cultural',icon: require('../assets/cultural.png'),key:10,}, 
+    {name:'Activism',icon: require('../assets/activism.png'),key:11,}, {name:'Music',icon: require('../assets/music.png'),key:12,}, 
+    {name:'Art/Design', icon: require('../assets/artdesign.png'),key:13,}, {name:'Food + Drink', icon: require('../assets/food.png'),key:14,}, 
+    {name:'Performance', icon: require('../assets/performance.png'),key:15,}, {name:'Presentation', icon: require('../assets/presentation.png'),key:16,}, 
+    {name:'Exhibition', icon: require('../assets/exhibition.png'),key:17,}, {name:'Academic', icon: require('../assets/academic.png'),key:18,},
+    {name:'Science/Tech', icon: require('../assets/science.png'),key:19,}, {name:'Business/Professional', icon: require('../assets/business.png'),key:20,},
+    {name:'Language/Literature', icon: require('../assets/language.png'),key:21,}, {name:'Religion', icon: require('../assets/religion.png'),key:22,},
+    {name:'Other', icon: require('../assets/other.png'),key:23,}];
+
     const windowHeight = Dimensions.get('window').height;
     bs = React.createRef();
     fall = new Animated.Value(1);
-    renderInner = () => (
-        <View style={styles.panel}>
-            <View>
-                <Text>
-                    {currentEvent.Name}
-                </Text>
-                <Text>
-                    lorem ipsum dolor sit amet
-                    consectetur adipiscing elit
-                    Morbi hendrerit ut felis
-                </Text>
-            </View>
-            <ScrollView>
-                <Text>
-                    THE BODY GOES HERE:
-                </Text>
-                <Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi hendrerit ut felis a gravida. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tincidunt lorem sit amet ex pretium, eget placerat ante gravida. Duis egestas tellus libero, fringilla consequat nunc porta sed. Morbi pulvinar ligula arcu, in ultrices lorem semper quis. Maecenas non turpis sed felis feugiat scelerisque sed vel sapien. Aliquam dolor nisl, ullamcorper at felis at, venenatis viverra ipsum. Maecenas non ipsum massa. Duis facilisis at enim vitae porttitor. Vivamus eget porttitor mauris. Curabitur nulla velit, vehicula semper lobortis eleifend, interdum id felis.
 
-Ut ornare est lorem, sit amet interdum nulla laoreet vel. Ut ut varius sapien, sit amet pharetra nibh. Vivamus nec arcu mattis, pretium neque et, vestibulum neque. Aenean fringilla laoreet maximus. Quisque in gravida sem, a ornare justo. Proin malesuada ligula dolor. In hac habitasse platea dictumst. Sed at auctor sem. Nam blandit orci libero, id tempor mi vestibulum et. In ultricies sagittis porta. Suspendisse vel ligula id sapien convallis varius. Morbi sem leo, accumsan a placerat sed, tempor in sapien. Etiam pharetra dui justo. Nullam vitae nunc mattis, suscipit urna et, fringilla justo. Nullam et metus lacus. Proin interdum vehicula tortor, eu pharetra dolor pharetra sit amet.
-
-Proin vel neque sed quam tincidunt fringilla. Sed in aliquet justo. Sed nec ex quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer odio turpis, gravida ac velit id, semper imperdiet ex. In sagittis at nulla at tempus. Morbi viverra lectus justo, eu ultricies leo euismod sed. Morbi non lectus eu purus iaculis tempor ac eget lorem. Curabitur dictum laoreet sapien, convallis consequat nibh volutpat ac. Duis tincidunt tortor tellus. Vivamus scelerisque purus est, sagittis egestas arcu tempor eget. Duis sed congue velit, aliquam vulputate nisi.
-
-Quisque pharetra justo libero, id luctus lacus imperdiet et. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean laoreet dictum orci, id volutpat urna varius a. Suspendisse orci mauris, dapibus vitae ultricies ut, tincidunt ac velit. Nulla eleifend ullamcorper sapien, ac pharetra justo placerat at. Quisque eu quam sagittis, hendrerit eros in, facilisis nunc. Suspendisse lacus massa, sodales a justo in, scelerisque mollis libero.
-
-Nam in arcu porta, volutpat neque et, finibus ligula. Donec suscipit placerat interdum. Fusce magna urna, iaculis ac tincidunt quis, malesuada ut leo. Mauris sed magna a erat laoreet dictum. Morbi viverra, enim non accumsan placerat, felis diam efficitur erat, nec pharetra quam orci imperdiet elit. Praesent accumsan congue tellus quis convallis. Donec in mi et enim dignissim consequat. Pellentesque venenatis, nunc at malesuada sollicitudin, elit eros aliquam elit, vel malesuada dolor nisi sed lacus. Curabitur egestas nisi tellus, nec venenatis urna tristique lacinia. Suspendisse consequat, sapien eu gravida posuere, ex libero condimentum enim, id pretium leo libero ut sapien. Sed varius eros quis velit gravida, ut venenatis magna suscipit. Nam fermentum erat vel lectus ornare suscipit. Pellentesque volutpat placerat tellus nec pulvinar. Aliquam consequat, nibh vel varius sollicitudin, elit metus pulvinar velit, vitae ultrices est lectus id erat.
-                </Text>
-            </ScrollView>
+    const renderCategories = () => {
+      let pic = ""
+      for (let i = 0; i < categories.length; i++) {
+        if (categories[i].name == currentEvent.MainCategory) {
+          pic = categories[i].icon
+        }
+      }
+      return (
+        <View style={{flexDirection: 'row'}}>
+          <Image
+            source={pic}
+            style={{width:18, height: 18}}>
+          </Image>
+          <Text style={{marginLeft: 5}}>{currentEvent.MainCategory}</Text>
         </View>
+      )
+    }
+    
+    const registration = () => {
+      if(currentEvent.Registration != '') {
+        return (
+          <View>
+            <Text style={{fontWeight: 'bold'}}>Registration</Text>
+            <Text>{currentEvent.Registration}</Text>
+          </View>
+        )
+      }
+    }
+
+    const moreDetails = () => {
+      if(currentEvent.Email != '' && currentEvent.Website != '') {
+        return (
+          <View>
+            <Text style={{fontWeight: 'bold'}}>More Details</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text>Email: </Text>
+              <Text>{currentEvent.Email}</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text>Website: </Text>
+              <Text>{currentEvent.Website}</Text>
+            </View>
+          </View>
+        )
+      } else if (currentEvent.Email != '') {
+        return (
+          <View>
+            <Text style={{fontWeight: 'bold'}}>More Details</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text>Email: </Text>
+              <Text>{currentEvent.Email}</Text>
+            </View>
+          </View>
+        )
+      } else if (currentEvent.Website != '') {
+        return (
+          <View>
+            <Text style={{fontWeight: 'bold'}}>More Details</Text>
+            <View style={{flexDirection: 'row'}}>
+              <Text>Website: </Text>
+              <Text>{currentEvent.Website}</Text>
+            </View>
+          </View>
+        )
+      }
+    }
+
+    const [buttonColor1, setButtonColor1] = useState('#D3D3D3')
+
+    const toggle1 = () => {
+      if (buttonColor1 == '#D3D3D3') {
+        setButtonColor1('#FFCB05')
+      } else {
+        setButtonColor1('#D3D3D3')
+      }
+    }
+
+    const [buttonColor2, setButtonColor2] = useState('#D3D3D3')
+
+    const toggle2 = () => {
+      if (buttonColor2 == '#D3D3D3') {
+        setButtonColor2('#FFCB05')
+      } else {
+        setButtonColor2('#D3D3D3')
+      }
+    }
+
+    const [buttonColor3, setButtonColor3] = useState('#D3D3D3')
+
+    const toggle3 = () => {
+      if (buttonColor3 == '#D3D3D3') {
+        setButtonColor3('#FFCB05')
+      } else {
+        setButtonColor3('#D3D3D3')
+      }
+    }
+
+    const [isTruncated, setIsTruncated] = useState(true);
+    const resultString = isTruncated ? currentEvent.Description.slice(0, 133) : currentEvent.Description;
+    const toggle = () => {
+      setIsTruncated(!isTruncated);
+    }
+
+    const renderButton = () => {
+      if (resultString.length > 130) {
+        return (
+          <TouchableOpacity onPress={toggle}>
+            <Text style={{color: '#FFCB05', marginBottom: 10}}>Read More</Text>
+          </TouchableOpacity>
+        )
+      }
+    }
+  
+    renderInner = () => (
+      <View style={styles.panel}>
+        <View style={{alignItems: 'center', flexDirection: 'row', justifyContent: 'flex-start', marginBottom: 10}}>
+          <View>
+            <Text style={{
+              fontSize: 24,
+              width: Dimensions.get('window').width - 100,
+              marginRight: 10    
+              }} 
+              numberOfLines={2}>
+                {currentEvent.Name}
+              </Text>
+          </View>
+          <View style={{backgroundColor: '#D3D3D3', borderRadius: 5, padding: 5, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: 'white'}}>{currentEvent.Privacy}</Text>
+          </View>
+          
+        </View>
+        <View style={styles.panelHost}>
+          <Image
+            source={require('../assets/Vector.png')}
+            style={{width:18, height: 18}}>
+          </Image>
+          <Text style={{marginLeft: 5, maxWidth: 200, marginRight: 15}}>{currentEvent.Organizer}</Text>
+          {renderCategories()}
+        </View>
+        <View style={styles.panelDate}>
+          <Image
+            source={require('../assets/CalendarIcon.png')}
+            style={{width:18, height:18}}
+          ></Image>
+          <Text style={{marginLeft: 5}}>{currentEvent.StartDayTime}</Text>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: 55 }}>
+          <TouchableOpacity style={{backgroundColor: buttonColor1,
+            borderRadius: 8,
+            width: (Dimensions.get('window').width - 81.6) / 3,
+            height: 55,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginHorizontal: 15,
+            }}
+            onPress={toggle1}>
+            <View>
+              <Image
+                source={require('../assets/star.png')}
+                style={{height:18, width: 18, alignSelf: 'center', tintColor: 'white'}}
+              ></Image>
+              <Text style={styles.panelButtonTitle}>Save</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={{backgroundColor: buttonColor2,
+            borderRadius: 8,
+            width: (Dimensions.get('window').width - 81.6) / 3,
+            height: 55,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginHorizontal: 15,
+            }}
+            onPress={toggle2}>
+            <View>
+              <Image
+                source={require('../assets/check2.png')}
+                style={{height:18, width: 18, alignSelf: 'center', tintColor: 'white'}}
+              ></Image>
+              <Text style={styles.panelButtonTitle}>I'm Going</Text>
+            </View>   
+          </TouchableOpacity>
+          <TouchableOpacity style={{backgroundColor: buttonColor3,
+            borderRadius: 8,
+            width: (Dimensions.get('window').width - 81.6) / 3,
+            height: 55,
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginHorizontal: 15,
+            }}
+            onPress={toggle3}>
+            <View>
+              <Image
+                source={require('../assets/share2.png')}
+                style={{height:18, width: 18, alignSelf: 'center', tintColor: 'white'}}
+              ></Image>
+              <Text style={styles.panelButtonTitle}>Share</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        
+        
+          <View>
+            <Image source={require('../assets/avatar.jpeg')}
+            resizeMode= 'cover'
+            style={{width: Dimensions.get('window').width - 40.8, height: 200, marginBottom: 20}}>
+            </Image>
+          </View>
+          <Text style={{fontWeight: 'bold'}}>Event Description</Text>
+          <View>
+            <Text>{resultString.replace(/(\r\n|\n|\r)/gm, " ")}</Text>
+            {renderButton()}
+          </View>
+          <Text style={{fontWeight: 'bold'}}>Location</Text>
+          <Text>{currentEvent.LocationName}</Text>
+          <Text style={{marginBottom: Dimensions.get('window').height}}>{currentEvent.Address}</Text>
+          {registration()}
+          {moreDetails()}
+        
+      </View>
     );
     
     renderHeader = () => (
@@ -104,6 +311,7 @@ Nam in arcu porta, volutpat neque et, finibus ligula. Donec suscipit placerat in
           </View>
         </View>
     );
+
     const [latDelta, setLatDelta] = useState(0.015);
     const [longDelta, setLongDelta] = useState(latDelta/2);
     
@@ -187,11 +395,11 @@ Nam in arcu porta, volutpat neque et, finibus ligula. Donec suscipit placerat in
           eventList.map((item) => {
             if(matchesCriteria(item)) {
             return (
-              <MapView.Marker key = {item.id}
+              <Marker key = {item.id}
               coordinate = {{latitude: parseFloat(item.Latitude), longitude: parseFloat(item.Longitude)}} 
               onPress = {() => openBottomSheet(item)}>
                 <LocationPin title = {item.Name}/>
-              </MapView.Marker>
+              </Marker>
             )
             }
           })
@@ -231,7 +439,7 @@ Nam in arcu porta, volutpat neque et, finibus ligula. Donec suscipit placerat in
             <View style={styles.pullup}>
                 <BottomSheet
                     ref={this.bs}
-                    snapPoints={[0, 300, windowHeight - 50]}
+                    snapPoints={[0, 270, windowHeight - 50]}
                     renderContent={this.renderInner}
                     renderHeader={this.renderHeader}
                     initialSnap={0}
@@ -304,7 +512,7 @@ const styles = StyleSheet.create({
         flex:1,
     },
     container: {
-        flex: 1, 
+        flex: 1,
     },
     topbar: {
         position: 'absolute',
@@ -336,44 +544,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         paddingTop: 20,
         //paddingBottom: Dimensions.get('window').height,
-    }
+    },
+    panelTitle: {
+      fontSize: 27,
+      height: 35,
+      marginRight: 10,
+      width: Dimensions.get('window').width - 100    
+    },
+    panelHost: {
+      flexDirection: 'row',
+      marginBottom: 10
+    },
+    panelDate: {
+      flexDirection: 'row',
+      marginBottom: 20
+    },
+    panelButtonTitle: {
+      fontSize: 17,
+      fontWeight: 'bold',
+      color: 'white',
+    },
 })
-
-/*
-const mapStyle  = [
-  {
-    "featureType": "administrative",
-    "elementType": "geometry",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "poi",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.icon",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  },
-  {
-    "featureType": "transit",
-    "stylers": [
-      {
-        "visibility": "off"
-      }
-    ]
-  }
-];
-*/

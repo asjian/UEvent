7e0a867cb52bf64c4eea9fe72044d511a888837f
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-08-03 22:52:19 -0400

    Update SignInScreen.js

--- Screens/SignInScreen.js
+++ Screens/SignInScreen.js
@@ -11,109 +11,19 @@ import {
     Image
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Feather from 'react-native-vector-icons/Feather';
 import { useTheme } from 'react-native-paper';
-import Users from '../dummies/users';
 import * as Google from "expo-google-app-auth";
 import Globals from '../../GlobalVariables';
 import AppContext from '../objects/AppContext';
 
 const SignInScreen = ({navigation}) => {
     const myContext = useContext(AppContext);
-    const [data, setData] = React.useState({
-        username: '',
-        password: '',
-        check_textInputChange: false,
-        secureTextEntry: true,
-        isValidUser: true,
-        isValidPassword: true,
-    });
 
     const { colors } = useTheme();
 
-    const textInputChange = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: true,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                username: val,
-                check_textInputChange: false,
-                isValidUser: false
-            });
-        }
-    }
-
-    const handlePasswordChange = (val) => {
-        if( val.trim().length >= 8 ) {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: true
-            });
-        } else {
-            setData({
-                ...data,
-                password: val,
-                isValidPassword: false
-            });
-        }
-    }
-
-    const updateSecureTextEntry = () => {
-        setData({
-            ...data,
-            secureTextEntry: !data.secureTextEntry
-        });
-    }
-
-    //ties in with back-end
-    const handleValidUser = (val) => {
-        if( val.trim().length >= 4 ) {
-            setData({
-                ...data,
-                isValidUser: true
-            });
-        } else {
-            setData({
-                ...data,
-                isValidUser: false
-            });
-        }
-    }
-
-    //checks if username or password is correct
-    const loginHandle = (userName, password) => {
-
-        const foundUser = Users.filter( item => {
-            return userName == item.username && password == item.password;
-        } );
-
-        if ( data.username.length == 0 || data.password.length == 0 ) {
-            Alert.alert('BRUH!', 'Username or password field cannot be empty.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-
-        if ( foundUser.length == 0 ) {
-            Alert.alert('Invalid User!', 'Username or password is incorrect.', [
-                {text: 'Okay'}
-            ]);
-            return;
-        }
-        signIn(foundUser);
-    }
-
     const addAndSaveUser = (existsInDataBase,user) => {
         const fetchurl = Globals.usersURL;
-        if(!existsInDataBase) { //then add to database, and store some default values in globals
+        if(!existsInDataBase) { //then add to database
             fetch(fetchurl, {
                 method: 'POST',
                 headers: {
@@ -184,88 +94,13 @@ const SignInScreen = ({navigation}) => {
                 backgroundColor: colors.background
             }]}
         >
-            <Text style={[styles.text_footer, {
-                color: colors.text
-            }]}>Username</Text>
-            <View style={styles.action}>
-                <FontAwesome 
-                    name="user-o"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput 
-                    placeholder="Your Username"
-                    placeholderTextColor="#666666"
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => textInputChange(val)}
-                    onEndEditing={(e)=>handleValidUser(e.nativeEvent.text)}
-                />
-                {data.check_textInputChange ? 
-                <Animatable.View
-                    animation="bounceIn"
-                >
-                    <Feather 
-                        name="check-circle"
-                        color="green"
-                        size={20}
-                    />
-                </Animatable.View>
-                : null}
+            <View>
+                <Text style={{color: '#05375a',
+                fontWeight: 'bold',
+                fontSize: 18}}> 
+                    Notice: This app is dedicated to umich students only. Please select your umich email when signing in with Google :)
+                </Text>
             </View>
-            { data.isValidUser ? null : 
-            <Animatable.View animation="fadeInLeft" duration={500}>
-            <Text style={styles.errorMsg}>Username must be atleast 4 characters long.</Text>
-            </Animatable.View>
-            }
-            
-
-            <Text style={[styles.text_footer, {
-                color: colors.text,
-                marginTop: 35
-            }]}>Password</Text>
-            <View style={styles.action}>
-                <Feather 
-                    name="lock"
-                    color={colors.text}
-                    size={20}
-                />
-                <TextInput 
-                    placeholder="Your Password"
-                    placeholderTextColor="#666666"
-                    secureTextEntry={data.secureTextEntry ? true : false}
-                    style={[styles.textInput, {
-                        color: colors.text
-                    }]}
-                    autoCapitalize="none"
-                    onChangeText={(val) => handlePasswordChange(val)}
-                />
-                <TouchableOpacity
-                    onPress={updateSecureTextEntry}
-                >
-                    {data.secureTextEntry ? 
-                    <Feather 
-                        name="eye-off"
-                        color="grey"
-                        size={20}
-                    />
-                    :
-                    <Feather 
-                        name="eye"
-                        color="grey"
-                        size={20}
-                    />
-                    }
-                </TouchableOpacity>
-            </View>
-            
-
-            <TouchableOpacity>
-                <Text style={{color: '#FFCB05', marginTop:15}}>Forgot password?</Text>
-            </TouchableOpacity>
-
             <View style={styles.button}>
                 <TouchableOpacity style={[styles.signIn, {
                         flexDirection: 'row',
@@ -283,32 +118,9 @@ const SignInScreen = ({navigation}) => {
                         marginLeft: 24,
                 }}>Sign In With Google</Text>
                 </TouchableOpacity>
-                {/*
-                <TouchableOpacity
-                    style={[styles.signIn, {
-                        borderColor: '#FFCB05',
-                        borderWidth: 1,
-                    }]}
-                    //onPress={() => {loginHandle( data.username, data.password )}}
-                    onPress={() => {navigation.navigate('WelcomeScreen')}}
-                >
-                    <Text style={[styles.textSign, {
-                        color:'#FFCB05'
-                    }]}>Sign In</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={() => navigation.navigate('SignUpScreen')}
-                    style={[styles.signIn, {
-                        marginTop: 15
-                    }]}
-                >
-                    
-                    <Text style={[styles.textSign, {
-                        color: '#FFCB05'
-                    }]}>Sign Up</Text>
-                </TouchableOpacity>
-                */}
+            </View>
+            <View>
+                <Image source={require('../assets/wolverine.png')}/>
             </View>
         </Animatable.View>
       </View>

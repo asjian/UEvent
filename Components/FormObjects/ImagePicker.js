775cd121eac584605346e14254cf9ee9c40a288b
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-08-03 22:53:00 -0400

    Update ImagePicker.js

--- Components/FormObjects/ImagePicker.js
+++ Components/FormObjects/ImagePicker.js
@@ -5,7 +5,7 @@ import * as ImagePicker from 'expo-image-picker';
 export default function ImagePickerExample({onChange, value}) {
   const [image, setImage] = useState(null);
 
-  useEffect(() => {
+  {/*useEffect(() => {
     (async () => {
       if (Platform.OS !== 'web') {
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -14,9 +14,20 @@ export default function ImagePickerExample({onChange, value}) {
         }
       }
     })();
-  }, []);
+  }, []);*/}
+
+  const permissionCall = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+  
 
   const pickImage = async () => {
+    permissionCall();
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,

9726c3e30a430e9d5882b9aee9985c9761e2896d
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-08-03 22:48:55 -0400

    Update HostScreen.js

--- Screens/HostScreen.js
+++ Screens/HostScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { Dimensions } from 'react-native';
 import HostNavBar from '../routes/HostNavBar';
 import CreateNewEventScreen from './CreateNewEvent';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import ManageEventScreen from './ManageEvent';
+import ManageEventStack from './ManageEvent';
 
 const Stack = createStackNavigator()
+
+const windowHeight = Dimensions.get('window').height;
+
 
 function HostScreen(props) {
     return (
@@ -14,17 +20,18 @@ function HostScreen(props) {
                 component={HostNavBar}
                 options={{
                     headerStyle: {
-                        height: 125,
+                        height: 100,
                         backgroundColor: '#FFFBF3'
                     },
                     headerTitleStyle: {
                         fontWeight: 'bold',
                         fontSize: 24,
-
-                    }
+                    },
+                    headerLeft: null,
                 }}
             />
             <Stack.Screen name="Create a New Event" component={CreateNewEventScreen} options={{ headerShown: false }} />
+            <Stack.Screen name='Manage Event' component={ManageEventStack} options={{ headerShown: false }}/>
         </Stack.Navigator>
     );
 }

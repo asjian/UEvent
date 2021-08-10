2560160d575bb0df1ea8f1a53f3769e2762eeb2d
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-08-09 21:48:58 -0400

    Update PastEvents.js

--- Screens/PastEvents.js
+++ Screens/PastEvents.js
@@ -1,6 +1,34 @@
-import React, { useContext } from 'react';
-import { SafeAreaView, View, Text, Button, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Button, StyleSheet, Image, ParentView, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import AppContext from '../objects/AppContext';
+import Globals from '../../GlobalVariables';
+//import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import ManageEventScreen from './ManageEvent';
+
+
+const EventBox = ({navigation, myContext, item}) => {
+    return (
+        
+            <TouchableOpacity onPress={() => {myContext.toggleShowNavBar(false);navigation.navigate('Manage Event', { screen: 'Manage Event', params: {item: item} });}} style={styles.box}>
+            <View style={{ flex: 2 }}>
+                <Image style={styles.realImageStyle} source={require('../assets/user_icon.png')} />
+            </View>
+            <View style={{ flex: 6 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={{ fontWeight: '500', fontSize: Globals.HR(13), color: '#0085FF', flex: 6 }}>{item.startTime} - {item.endTime}</Text>
+                    <Image style={{ resizeMode: 'contain', flex: 1 }} source={require('../assets/NotificationBell.png')} />
+                </View>
+                <Text style={{ fontWeight: "500", fontSize: Globals.HR(16), marginBottom: '3%' }}>{item.name}</Text>
+                <Text style={{ fontWeight: '500', fontSize: Globals.HR(13), color: '#09189F' }}>{item.locationName}</Text>
+            </View>
+            
+            </TouchableOpacity>
+        
+        
+    );
+}
+
+
 
 
 function PastEventsScreen({ navigation }) {
@@ -10,40 +38,124 @@ function PastEventsScreen({ navigation }) {
         myContext.toggleShowNavBar(false);
         navigation.navigate('Create a New Event');
     }
+
+    const [PastEvents, setPastEvents] = useState([]);
+
+    // const getEvents = () => {
+    //     console.log('fetching past events...');
+    //     let fetchurl = Globals.eventsURL;
+    //     fetch(fetchurl)
+    //       .then((response) => response.json())
+    //       .then((json) => {setUpcomingEvents(json)})
+    //       .catch((error) => console.error(error))
+    // }
+    //   const [fetched,setFetched] = useState(false);
+
+    //     useEffect(() => {
+
+    //             getEvents();
+    //             setFetched(true);
+            
+    //     },[]);
+        
+    const renderItem = ({ item }) => (
+        <EventBox item={item} navigation={navigation} myContext={myContext}  />
+    );
+
+    const EmptyListMessage = () => {
+		return (
+            <View style={{alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={{fontSize: Globals.HR(24), textAlign: 'center', padding: Globals.HR(20), justifyContent: 'center', flex: 1, fontWeight: '500', color: 'rgba(0, 0, 0, 0.5)', width: '80%'}}>You have not organized any events in the past</Text>
+            </View>
+			
+		);
+	}
+
     return (
-        <SafeAreaView style={{ backgroundColor: '#FFFBF3', width: '100%', height: '100%' }}>
+
+
+
+        <SafeAreaView style={{ backgroundColor: '#FFFBF3', height: '100%' }}>
             <View style={{ height: '60%' }}>
 
+                
 
-                <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-
-                    {/*<Text style={{ fontSize: 25, fontWeight: '500', color: '#000000' }}>You have not organized any events in the past</Text>*/}
-
-
-
-
-                </ScrollView>
+                    <FlatList 
+                        data={PastEvents}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id.toString()}
+                        style={{height: '100%'}}
+                        ListEmptyComponent={EmptyListMessage}
+                    />
+                
+                
             </View>
-            <View >
+            <View style={styles.NewEventButton}>
                 <TouchableOpacity onPress={createEventHandler}>
                     <View style={styles.selectContainer}>
                         <Text style={styles.selectText}>+ Create New Event</Text>
                     </View>
                 </TouchableOpacity>
             </View>
+
         </SafeAreaView>
+
+
+
     );
 }
+
 
 export default PastEventsScreen;
 
 const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: '100%',
+        padding: Globals.HR(5),
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+    },
+    NewEventButton: {
+        backgroundColor: '#FFFBF3',
+        flex: 1
+    },
+    box: {
+        width: '95%',
+        padding: Globals.HR(10),
+        margin: Globals.HR(10),
+        flexDirection: 'row',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowColor: '#000000',
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+        backgroundColor: '#F8F8F8',
+        flex: 1
+
+    },
+    inner1: {
+        flex: 2,
+
+    },
+    inner2: {
+        flex: 3
+    },
+
+    realImageStyle: {
+        height: '100%',
+        resizeMode: 'contain',
+        width: '100%'
+
+    },
     selectContainer: {
         backgroundColor: '#ffffff',
         position: 'absolute',
-        marginHorizontal: 50,
-        marginTop: 5,
+        marginHorizontal: Globals.WR(50),
+        marginTop: Globals.HR(5),
         width: '75%',
         alignItems: 'center',
         top: 0,
@@ -55,12 +167,13 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 1.41,
         elevation: 2,
-        borderRadius: 10,
+        borderRadius: Globals.HR(10),
     },
     selectText: {
-        fontWeight: 'bold',
-        fontSize: 22,
-        paddingVertical: 15,
+        fontWeight: '500',
+        fontSize: Globals.HR(22),
+        paddingVertical: Globals.HR(15),
         color: '#fab400',
     }
+
 })

55fe6a979e768dd0487080e5feffcb7322d53e3b
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-08-09 22:50:23 -0400

    Update HostScreen.js

--- Screens/HostScreen.js
+++ Screens/HostScreen.js
@@ -1,48 +1,44 @@
-import React, {useContext} from 'react';
+import React, {useEffect} from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Dimensions,TouchableOpacity,Text } from 'react-native';
+import { Dimensions } from 'react-native';
 import HostNavBar from '../routes/HostNavBar';
 import CreateNewEventScreen from './CreateNewEvent';
 //import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import ManageEventScreen from './ManageEvent';
 import ManageEventStack from './ManageEvent';
-import AppContext from '../objects/AppContext';
 
 const Stack = createStackNavigator()
 
-const windowHeight = Dimensions.get('window').height;
+
+
+// 926
+
+
+
 
 function HostScreen(props) {
-    const myContext = useContext(AppContext)
+    const windowHeight = Dimensions.get('window').height;
+    // useEffect(() => {
+
+    //     console.log(windowHeight);
+    
+    // },[]);
     return (
         <Stack.Navigator>
             <Stack.Screen
                 name="Events I'm Hosting"
                 component={HostNavBar}
-                options = {({navigation}) => ({
+                options={{
                     headerStyle: {
-                        height: 90,
-                        backgroundColor: '#FFFBF3',
-                        shadowColor: 'transparent',
+                        height: windowHeight / 9,
+                        backgroundColor: '#FFFBF3'
                     },
                     headerTitleStyle: {
                         fontWeight: 'bold',
-                        fontSize: 24,
-                        marginLeft: -5,
+                        fontSize: windowHeight / 38.58,
                     },
                     headerLeft: null,
-                    /*
-                    headerRight: () => {
-                        return (
-                            <TouchableOpacity onPress = {() => {myContext.toggleShowNavBar(false);navigation.navigate('Create a New Event')}}>
-                                <Text style = {{fontWeight:'bold',fontSize:16,color:'#0085ff',marginRight:20,marginTop:2,}}>+Create</Text>
-                            </TouchableOpacity>
-                        )
-                    },  
-                    */
-                    
-                })          
-                }
+                }}
             />
             <Stack.Screen name="Create a New Event" component={CreateNewEventScreen} options={{ headerShown: false }} />
             <Stack.Screen name='Manage Event' component={ManageEventStack} options={{ headerShown: false }}/>

b79a3696311dc63b6aa707572de4d33a868db071
sundeep-ravella <72945547+sundeep-ravella@users.noreply.github.com>
2021-08-09 22:49:35 -0400

    Update UpcomingEvents.js

--- Screens/UpcomingEvents.js
+++ Screens/UpcomingEvents.js
@@ -1,9 +1,12 @@
-import React, { useContext, useState, useEffect,useRef } from 'react';
-import { SafeAreaView, View, Text, Animated, StyleSheet, Image, TouchableHighlight, TouchableOpacity, ScrollView, FlatList, Dimensions } from 'react-native';
+import React, { useContext, useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, Button, StyleSheet, Image, ParentView, TouchableOpacity, ScrollView, FlatList, Dimensions } from 'react-native';
 import AppContext from '../objects/AppContext';
 import Globals from '../../GlobalVariables';
 //import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import ManageEventScreen from './ManageEvent';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
 
 const EventBox = ({navigation, myContext, item}) => {
     return (
@@ -12,13 +15,13 @@ const EventBox = ({navigation, myContext, item}) => {
             <View style={{ flex: 2 }}>
                 <Image style={styles.realImageStyle} source={require('../assets/user_icon.png')} />
             </View>
-            <View style={{ flex: 4 }}>
+            <View style={{ flex: 6 }}>
                 <View style={{ flexDirection: 'row' }}>
-                    <Text style={{ fontWeight: '500', fontSize: 13, color: '#09189F', marginBottom: '5%' }}>{item.startTime} - {item.endTime}</Text>
-                    <Image style={{ resizeMode: 'contain', marginLeft: '10%' }} source={require('../assets/NotificationBell.png')} />
+                    <Text style={{ fontWeight: '500', fontSize: windowHeight / 71.23, color: '#0085FF', flex: 6 }}>{item.startTime} - {item.endTime}</Text>
+                    <Image style={{ resizeMode: 'contain', flex: 1 }} source={require('../assets/NotificationBell.png')} />
                 </View>
-                <Text style={{ fontWeight: "500", fontSize: 16, marginBottom: '3%' }}>{item.name}</Text>
-                <Text style={{ fontWeight: '500', fontSize: 13, color: '#0085FF' }}>{item.LocationName}</Text>
+                <Text style={{ fontWeight: "500", fontSize: windowHeight / 57.88, marginBottom: '3%' }}>{item.name}</Text>
+                <Text style={{ fontWeight: '500', fontSize: windowHeight / 71.23, color: '#09189F' }}>{item.locationName}</Text>
             </View>
             
             </TouchableOpacity>
@@ -26,6 +29,10 @@ const EventBox = ({navigation, myContext, item}) => {
         
     );
 }
+
+
+
+
 function UpcomingEventsScreen({ navigation }) {
     const myContext = useContext(AppContext);
     // event handler function
@@ -47,70 +54,46 @@ function UpcomingEventsScreen({ navigation }) {
       const [fetched,setFetched] = useState(false);
 
         useEffect(() => {
-          if(!fetched) {
-            getEvents();
-            setFetched(true);
-          }
-        });
 
+                getEvents();
+                setFetched(true);
+            
+        },[]);
+        
     const renderItem = ({ item }) => (
         <EventBox item={item} navigation={navigation} myContext={myContext}  />
     );
-    /*
-    const [visible,setVisible] = useState(true);
-    const [offset,setOffset] = useState(0);
-    const [up,setUp] = useState(false);
-    const fadeAnim = useRef(new Animated.Value(1)).current;
-    
-    const fadeIn = () => {
-      // Will change fadeAnim value to 1 in 5 seconds
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 300,
-        useNativeDriver: true,
-      }).start();
-    };
-    const fadeOut = () => {
-      // Will change fadeAnim value to 0 in 3 seconds
-      Animated.timing(fadeAnim, {
-        toValue: 0,
-        duration: 100,
-        useNativeDriver: true
-      }).start(({finished}) => {if(finished)setVisible(false)});
-    };
-    
-    const handleVisibility = (nativeEvent) => {
-        const currentOffset = nativeEvent.contentOffset.y;
-        console.log('currentOffset: ' + currentOffset);
-        //console.log('offset: ' + offset);
-        setUp(currentOffset<offset?true:false);
-        setOffset(currentOffset);
-    }
-    */
+
     return (
+
+
+
         <SafeAreaView style={{ backgroundColor: '#FFFBF3', height: '100%' }}>
-            <View style={{ height: '92%' }}>
+            <View style={{ height: '80%' }}>
+
+                
 
                     <FlatList 
                         data={UpcomingEvents}
                         renderItem={renderItem}
                         keyExtractor={item => item.id.toString()}
                         style={{height: '100%'}}
-                        //onScroll = {({nativeEvent}) => handleVisibility(nativeEvent)}
-                        //onMomentumScrollBegin = {() => {console.log('momentum');if(up){setVisible(true);fadeIn()}}}
-                        //onEndReachedThreshold = {0.01}
-                        //onEndReached = {() => {console.log('maybe end');if(offset > 5){console.log('end');fadeOut()}}}
-                        contentContainerStyle = {{paddingTop:20,paddingBottom:80}}
-                    />   
-                    
-                    <TouchableHighlight activeOpacity = {0.7} onPress={createEventHandler}>
-                        <View style = {styles.selectContainer}>
-                            <Text style={[styles.selectText,{paddingHorizontal:50}]}>+ Create New Event</Text>
-                        </View>
-                    </TouchableHighlight>
-                    
+                    />
+                
+                
             </View>
+            <View style={styles.NewEventButton}>
+                <TouchableOpacity onPress={createEventHandler}>
+                    <View style={styles.selectContainer}>
+                        <Text style={styles.selectText}>+ Create New Event</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
         </SafeAreaView>
+
+
+
     );
 }
 
@@ -123,21 +106,23 @@ UpcomingEventsScreen.navigationOptions = {
 
 export default UpcomingEventsScreen;
 
+
 const styles = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        padding: 5,
+        padding: windowHeight / 185.2,
         flexDirection: 'column',
         flexWrap: 'wrap',
     },
     NewEventButton: {
-        backgroundColor: '#FFFBF3'
+        backgroundColor: '#FFFBF3',
+        flex: 1
     },
     box: {
         width: '95%',
-        padding: 10,
-        marginBottom: 20,
+        padding: windowHeight / 92.6,
+        margin: windowHeight / 92.6,
         flexDirection: 'row',
         shadowOffset: {
             width: 0,
@@ -167,35 +152,26 @@ const styles = StyleSheet.create({
     },
     selectContainer: {
         backgroundColor: '#ffffff',
-        
         position: 'absolute',
-        left: 52,
-        bottom: 20,
-        
+        marginHorizontal: windowWidth / 8.52,
+        marginTop: windowHeight / 185.2,
         width: '75%',
         alignItems: 'center',
-        shadowColor: "#000",
+        top: 0,
         shadowOffset: {
             width: 0,
-            height: 2,
+            height: windowHeight / 926,
         },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        borderRadius: 50,
-    },
-    selectInvisible: {
-        backgroundColor: '#ffffff',
-        position: 'absolute',
-        marginHorizontal: 50,
-        bottom: 20,
-        width: '0%',
-        alignItems: 'center',
+        shadowColor: '#000000',
+        shadowOpacity: 0.2,
+        shadowRadius: windowHeight / 656.74,
+        elevation: windowHeight / 463,
+        borderRadius: windowHeight / 92.6,
     },
     selectText: {
-        fontWeight: '600',
-        fontSize: 22,
-        paddingVertical: 15,
+        fontWeight: '500',
+        fontSize: windowHeight / 42.09,
+        paddingVertical: windowHeight / 61.73,
         color: '#fab400',
     }
 

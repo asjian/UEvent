571c1595ad66f1dc474983621d5004957444bbdf
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-08-15 18:33:28 -0400

    Update UpcomingEventButton.js

--- Components/UpcomingEventButton.js
+++ Components/UpcomingEventButton.js
@@ -4,31 +4,32 @@ import {StyleSheet, Image, TouchableOpacity, Text, View, Dimensions,} from 'reac
 
 export default function ProfileButton({onPress, title, location, time}) {
     return (
-        <TouchableOpacity onPress = {onPress}>
-            <View style={{flexDirection: 'row'}}>
-                <View style = {styles.button}>
-                    <Text style = {styles.buttonText}> {title} </Text>
-                    <View style={{flexDirection: 'row'}}>
-                        <Image
-                            source={require('../assets/Vector.png')}
-                            style={{width:16, height: 16, marginLeft: 5}}>
-                        </Image>
-                        <Text style = {{marginBottom: 5, color: 'orange', fontSize: 16, marginRight: 10, fontWeight: 'bold'}}> {location} </Text>
-                        <Image
-                            source={require('../assets/CalendarIcon.png')}
-                            style={{width:16, height: 16}}>
-                        </Image>
-                        <Text style = {{marginBottom: 5, color: '#03a9f4', fontSize: 16, fontWeight: 'bold'}}> {time} </Text>
+        <View style={styles.block}>
+            <TouchableOpacity onPress = {onPress}>
+                <View style={{flexDirection: 'row'}}>
+                    <View style = {styles.button}>
+                        <Text style = {styles.buttonText}> {title} </Text>
+                        <View style={{flexDirection: 'column'}}>
+                            <View style={{flexDirection: 'row'}}>
+                            <Image
+                                source={require('../assets/Vector.png')}
+                                style={{width:16, height: 16, marginLeft: 5}}>
+                            </Image>
+                            <Text style = {{marginBottom: 5, color: 'orange', fontSize: 16, marginRight: 10, fontWeight: 'bold'}}> {location} </Text>
+                            </View>
+                            <View style={{flexDirection: 'row'}}>
+                            <Image
+                                source={require('../assets/CalendarIcon.png')}
+                                style={{width:16, height: 16, marginLeft: 5}}>
+                            </Image>
+                            <Text style = {{marginBottom: 5, color: '#03a9f4', fontSize: 16, fontWeight: 'bold'}}> {time} </Text>
+                            </View>
+                        </View>
                     </View>
                 </View>
-                <View style={{justifyContent: 'center'}}>
-                    <Image source = {require('../assets/rightArrow.png')}
-                    style = {{width: 20, height: 20}}>
-                    </Image>
-                </View>
-            </View>
-            <View style={{borderBottomColor: 'black', borderBottomWidth: 1}}/>
-        </TouchableOpacity>
+            </TouchableOpacity>
+        </View>
+        
     );
 }
 const styles = StyleSheet.create({
@@ -41,11 +42,19 @@ const styles = StyleSheet.create({
     },
     button: {
         flex: 1,
-        width: Dimensions.get('window').width - 50
     },
     icon: {
         flex: 1,
         alignItems: 'flex-end',
         justifyContent: 'center'
-    }
+    },
+    block: {
+        backgroundColor: 'white',
+        marginBottom: 5,
+        borderRadius: 5,
+        shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 1, 
+    },
 })

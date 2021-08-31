3eef188daa3f2a3f9010aa7f63df7b38a2788ebc
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-08-30 23:36:34 -0400

    Update ImagePicker.js

--- Components/FormObjects/ImagePicker.js
+++ Components/FormObjects/ImagePicker.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { Button, Image, View, Platform, StyleSheet, TouchableOpacity, Text, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import Globals from '../../../GlobalVariables';
 
-export default function ImagePickerExample({onChange, value}) {
+export default function ImagePickerExample({onChange, value, editMode}) {
   const [image, setImage] = useState(null);
 
   {/*useEffect(() => {
@@ -20,7 +20,7 @@ export default function ImagePickerExample({onChange, value}) {
   const permissionCall = async () => {
     if (Platform.OS !== 'web') {
       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== 'granted') {
+      if (status != 'granted') {
         alert('Sorry, we need camera roll permissions to make this work!');
       }
     }
@@ -28,50 +28,49 @@ export default function ImagePickerExample({onChange, value}) {
   
 
   const pickImage = async () => {
-    permissionCall();
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [2, 1],
-      quality: 0.5,
-      base64: true
-    });
-
-    if (!result.cancelled) {
-      onChange('EventImage', result.uri);
-      setImage(result.uri);
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status != 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+      else {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [2, 1],
+          quality: 1,
+          base64: true
+        });     
+      if (!result.cancelled) {
+        if(result.base64.length > 10000000) {
+          Alert.alert("Image Too Large", "Your image is too large for us to handle, please choose a smaller one");
+        }
+        else {
+          onChange('EventImage', result.uri);
+          onChange('EventImageSize', result.base64.length);
+          onChange('EventImageDimensions', {height: result.height, width: result.width})
+          setImage(result.uri);
+          if(editMode) {
+            onChange('ImageEdited',true);
+          }
+        }
+      }
+      }
     }
   };
 
   const deleteImage = () => {
     setImage(null);
     onChange('EventImage', '');
+    if(editMode)
+      onChange('ImageEdited',true);
   }
-
-  return (
-    <View style={{margin: 30}}>
-      {value == '' && 
-
-      (<View style={{alignItems: 'center', justifyContent: 'center', width: '95%', height: 180, backgroundColor: 'rgba(248, 248, 248, 1)'  }}>
-        <TouchableOpacity onPress={pickImage} style={{position: 'absolute', top: 70, left: 90, zIndex: 1 }}>
-          <View style={styles.selectContainer}>
-              <Text style={styles.selectText}>Upload Photo</Text>
-          </View>
-        </TouchableOpacity>
-        {value == '' ? (null) 
-        : (value)  && 
-        <Image source={{ uri: value }} style={styles.image} />}
-      </View>)
-      }
-      {value !== '' && 
-
-        (<View style={{alignItems: 'center', justifyContent: 'center', width: '95%', height: 200,}}>
-          <TouchableOpacity onPress={deleteImage} style={{position: 'absolute', top: 5, left: 317, zIndex: 1 }}>
-              <View style={{height: 25, width: 25, backgroundColor: 'white', alignItems: 'center'}}>
-                  <Text style={{color:'black', fontSize: 20}}>X</Text>
-              </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={pickImage} style={{position: 'absolute', top: 90, left: 90, zIndex: 1 }}>
+    return (
+      <View style={{margin: 30, flex:1,}}>
+        {value == '' && 
+  
+        (<View style={{alignItems: 'center', alignSelf:'center',flex: 1, justifyContent: 'center', width: Globals.WR(320), height: Globals.HR(210), backgroundColor: 'rgba(248, 248, 248, 1)'  }}>
+          <TouchableOpacity onPress={pickImage} style={{position: 'absolute', justifyContent: 'center', alignSelf: 'center', zIndex: 1 }}>
             <View style={styles.selectContainer}>
                 <Text style={styles.selectText}>Upload Photo</Text>
             </View>
@@ -79,20 +78,36 @@ export default function ImagePickerExample({onChange, value}) {
           {value == '' ? (null) 
           : (value)  && 
           <Image source={{ uri: value }} style={styles.image} />}
-
         </View>)
         }
-    </View>
-    
-  );
+        {value !== '' && 
+  
+          (<View style={{alignItems: 'center', alignSelf: 'center', flex: 1, justifyContent: 'center', width: Globals.WR(320), height: Globals.HR(210),}}>
+            <TouchableOpacity onPress={deleteImage} style={{position: 'absolute', top: 0, right: 0, zIndex: 1 }}>
+                <View style={{height: 25, width: 25, backgroundColor: 'white', alignItems: 'center'}}>
+                    <Text style={{color:'black', fontSize: 20}}>X</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={pickImage} style={{position: 'absolute', justifyContent: 'center', alignSelf: 'center', zIndex: 1 }}>
+              <View style={styles.selectContainer}>
+                  <Text style={styles.selectText}>Upload Photo</Text>
+              </View>
+            </TouchableOpacity>
+            {value == '' ? (null) 
+            : (value)  && 
+            <Image source={{ uri: value }} style={styles.image} />}
+          </View>)
+          }
+      </View>   
+    );
 }
 
 const styles = StyleSheet.create({
   image: {
-    width: '95%',
-    height: Globals.HR(200),
+    width: Globals.WR(320),
+    height: Globals.HR(210),
     overflow: 'hidden',
-    marginTop: Globals.HR(20)
+    marginRight: 1
   },
   selectContainer: {
     backgroundColor: '#ffffff',

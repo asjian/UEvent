718df471e37b51858d09434e9f1eef9a56cd26a4
jzhou1402 <60801262+jzhou1402@users.noreply.github.com>
2021-08-30 23:36:50 -0400

    Update PrivacySelector.js

--- Components/FormObjects/PrivacySelector.js
+++ Components/FormObjects/PrivacySelector.js
@@ -4,7 +4,6 @@ import RNPickerSelect from 'react-native-picker-select';
 import { StyleSheet } from 'react-native';
 import Globals from '../../../GlobalVariables';
 
-
 const PrivacySelector = ({onChange, value}) => {
     const [isFocused5, setFocus5] = useState(false);
     return (
@@ -18,20 +17,25 @@ const PrivacySelector = ({onChange, value}) => {
             ]}
             style={{inputIOS: {borderWidth: 1,
                 borderColor: value !== null || isFocused5 ? '#7b7b7b' : '#C4C4C4',
-                padding: Globals.HR(8),
+                borderWidth: Globals.HR(1.2),
+                paddingVertical: 10,
+                paddingHorizontal: 6,
                 width: '88%',
-                marginLeft: Globals.WR(20), 
-                marginTop: Globals.HR(10),
-                marginBottom: Globals.HR(10),
+                marginLeft: 23,
+                marginTop: Globals.HR(10), 
+                marginBottom: Globals.HR(12),
                 flex: 1,
-                fontSize: Globals.HR(14)}}}
+                fontSize: Globals.HR(16),
+                },
+                placeholder: {
+                    color: '#a3a3a3',
+                },}}
             
             onOpen={() => setFocus5(true)}
             onClose={() => setFocus5(false)}
             placeholder={{
                 label: 'Public/Private',
                 value: null,
-                
             }}
         />
     );
@@ -39,17 +43,7 @@ const PrivacySelector = ({onChange, value}) => {
 
 export default PrivacySelector;
 
-const styles = StyleSheet.create({
-    InputBox: {
-        borderWidth: 0,
-        borderBottomWidth: 1,
-        borderColor: '#C4C4C4',
-        padding: 8,
-        width: '80%',
-        margin: 10
-    }
-})
-
+//not in use
 const pickerSelectStyles = StyleSheet.create({
     inputIOS: {
         borderWidth: 1,

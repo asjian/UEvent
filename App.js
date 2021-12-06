ea757b771243eda07da6283a7e163f3bcc0cf537
asjian <79596814+asjian@users.noreply.github.com>
2021-12-06 15:45:32 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,36 +1,63 @@
-import React, {useState,useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { StatusBar, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 //import NavBar from './app/routes/navBar';
 import AppContext from './app/objects/AppContext';
 import InitialNav from './app/routes/initialNav';
 import Globals from './GlobalVariables';
+import * as Linking from 'expo-linking';
+import * as TaskManager from 'expo-task-manager';
+import * as Notifications from 'expo-notifications';
 
 const App = () => {
-  const [showNavBar,setShowNavBar] = useState(true);
+  const lastNotificationResponse = Notifications.useLastNotificationResponse();
+  useEffect(() => {
+    if (
+      lastNotificationResponse &&
+      lastNotificationResponse.notification.request.content.data.eventId && lastNotificationResponse.notification.request.content.data.type &&
+      lastNotificationResponse.actionIdentifier === Notifications.DEFAULT_ACTION_IDENTIFIER
+    ) {
+      console.log('notification response caught!!');
+      updateNotification({
+        eventId: lastNotificationResponse.notification.request.content.data.eventId,
+        type: lastNotificationResponse.notification.request.content.data.type
+      });
+    }
+  }, [lastNotificationResponse]);
+  const [showNavBar, setShowNavBar] = useState(true);
   const toggleShowNavBar = (value) => {
     setShowNavBar(value);
   }
 
-  const [user,setUser] = useState({id: -1,displayName: '',email: 'nan@umich.edu',});
+  const [user, setUser] = useState({ id: -1, displayName: '', email: 'nan@umich.edu', });
   const initializeUser = (value) => {
     setUser(value);
   }
-  
-  const [mapSearchText,setMapSearchText] = useState(''); //NOTHING SHOULD TOUCH THIS EXCEPT MAPSEARCHBAR AND THE CLEAR BUTTON
+
+  const [mapSearchText, setMapSearchText] = useState(''); //NOTHING SHOULD TOUCH THIS EXCEPT MAPSEARCHBAR AND THE CLEAR BUTTON
   const changeMapSearchText = (value) => {
     setMapSearchText(value);
   }
 
-  const [eventList,setEventList] = useState([]);
+  const [eventList, setEventList] = useState([]);
   const updateEventList = (value) => {
     setEventList(value);
   }
 
-  const [postedEvent,setPostedEvent] = useState({inUse:false});
+  const [postedEvent, setPostedEvent] = useState({ inUse: false });
   const changePostedEvent = (value) => {
     setPostedEvent(value);
   }
 
+  const [originalEventLocation, setOriginalEventLocation] = useState({ latitude: 42.278, longitude: -83.738, inUse: false });
+  const changeOriginalEventLocation = (value) => {
+    setOriginalEventLocation(value);
+  }
+
+  const [notification, setNotification] = useState(null);
+  const updateNotification = (value) => {
+    setNotification(value);
+  }
   const globals = {
     navBarVisible: showNavBar,
     toggleShowNavBar,
@@ -46,55 +73,56 @@ const App = () => {
 
     postedEvent: postedEvent,
     changePostedEvent,
+
+    originalEventLocation: originalEventLocation,
+    changeOriginalEventLocation,
+
+    notification: notification,
+    updateNotification,
   }
-  const [fetchedCategories,setFetchedCategories] = useState(false);
-  //prepare things:
-      //fetch categories
-      if(!fetchedCategories) {
-        console.log('fetching categories...')
-        fetch(Globals.categoriesURL)
-        .then((response) => response.json())
-        .then((json) => {Globals.categories = json;setFetchedCategories(true)})
-        .catch((error) => console.error(error));
+  const [fetchedCategories, setFetchedCategories] = useState(false);
+  const adjust = (catArr) => {
+    let adjArr = [];
+    let otherId = -1;
+    for (let i = 0; i < catArr.length; i++) {
+      if (catArr[i].name != 'Other') {
+        adjArr.push(catArr[i])
       }
-  //mess with the database/api here
-  
-  /*
-  fetch('http://47.252.19.227/EventHub/rest/invitees/delete?eventId=10&userId=3', {
-    method: 'delete',
-  })
-  */
-  
-  /*
-  fetch('http://47.252.19.227/EventHub/rest/users/delete/8',{
-   method: 'delete',
-  });
-  */
-  
-  
-  
-/*
- fetch('https://retoolapi.dev/lvF3hn/events/4',{
-   method: 'patch',
-   headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-   },
-   body: JSON.stringify({
-      Attendees: "1 2 3",
-   })
- });
- */
-  /*
-  fetch('https://retoolapi.dev/CNVOvx/collisionevents/3', {
-    method:'delete',
-  }); 
-  */
+      else {
+        otherId = catArr[i].id;
+      }
+    }
+    if (otherId != -1)
+      adjArr.push({ id: otherId, name: 'Other' });
+
+    return adjArr;
+  }
+  const compareCats = (firstEl,secondEl) => {
+    return firstEl.priority > secondEl.priority;
+  }
+  //fetch categories
+  if (!fetchedCategories) {
+    Notifications.setNotificationHandler({
+      handleNotification: async (notification) => {  
+        return ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: false,
+        })
+      },
+    });
+    fetch(Globals.categoriesURL)
+      .then((response) => response.json())
+      .then((json) => { Globals.categories = json.sort(compareCats); setFetchedCategories(true) })
+      .catch((error) => console.error(error));
+    }
+  StatusBar.setBarStyle('dark-content', true);
+
   return (
     //<WelcomeNavigator />
-    <AppContext.Provider value = {globals}>
+    <AppContext.Provider value={globals}>
       <NavigationContainer>
-        <InitialNav/>
+        <InitialNav />
       </NavigationContainer>
     </AppContext.Provider>
     //<WelcomeScreen/>

dd6097e89e8d1e5f4fbe289061efc59c4d00a401
asjian <79596814+asjian@users.noreply.github.com>
2021-12-06 15:53:21 -0500

    Delete Components directory

--- Components/CloseButton.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import { SafeAreaView, View, Text, Settings, TouchableOpacity, StyleSheet, Image, Modal, Button, setItems } from 'react-native';
-
-// Close button used within a modal - change onPress functionality to fit your needs
-
-<Button title="X" onPress={() => setModalVisible(!modalVisible)} />
--- Components/users.js
+++
@@ -1,6 +0,0 @@
-export default Users = [
-    {key: 0, id:0, name: 'Kartikeya Gupta', email: 'kgupta@umich.edu'},
-    {key: 1, id:1, name: 'Alex Jian', email: 'asjian@umich.edu'},
-    {key: 2, id:2, name: 'Sundeep Ravella', email: 'sravella@umich.edu'},
-    {key: 3, id:3, name: 'Jiyang Zhou', email: 'jojiyang@umich.edu'}
-];

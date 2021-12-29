f62cc8936a279c91d01c5923d3d0925e92443d78
asjian <79596814+asjian@users.noreply.github.com>
2021-12-29 00:47:09 -0500

    Update notisender.py

--- notisender.py
+++ notisender.py
@@ -15,15 +15,26 @@ def getEvent(eventId):
 
 def getPushTokens():
     #Currently lack way of obtaining all user pushTokens, so going to return a hardcoded array for now
-    return ['ExponentPushToken[y4tqnCExADJYMmR_vcH5RI]', 'ExponentPushToken[28DZa9PZFiMgh9H5ulMXtv]', 'ExponentPushToken[YHji26KT6-9Cjp8YxvfFbu]', 
-            'ExponentPushToken[nQdtRBOEbFh9zLPYJwn5Gf]']
+    return [{'userId':5, 'pushToken':'ExponentPushToken[y4tqnCExADJYMmR_vcH5RI]'}, 
+            {'userId':5, 'pushToken':'ExponentPushToken[28DZa9PZFiMgh9H5ulMXtv]'},
+            {'userId':5, 'pushToken':'ExponentPushToken[YHji26KT6-9Cjp8YxvfFbu]'},
+            {'userId':5, 'pushToken':'ExponentPushToken[nQdtRBOEbFh9zLPYJwn5Gf]'}]
+
+def handleErrors(pushResponse, pushTokens):
+    if 'data' in pushResponse:
+        for i in range(len(pushResponse['data'])):
+            response = pushResponse['data'][i]
+            if response['status'] == 'error':
+                if response['details']['error'] == 'DeviceNotRegistered':
+                    print('deleting bad token')
+                    requests.delete(usersURL + '/deletePushToken', headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}, data = json.dumps({'pushToken': pushTokens[i]['pushToken']}))
 
 def sendNotis(pushTokens, eventjson):
     pushArr = []
-    for token in pushTokens:
+    for tokendict in pushTokens:
         pushArr.append(
             {
-                'to': token,
+                'to': tokendict['pushToken'],
                 'sound': 'default',
                 'title': 'Trending Event',
                 'body': eventjson['name'] + ' is trending! Tap to learn more about this event.',
@@ -42,19 +53,33 @@ def sendNotis(pushTokens, eventjson):
     postResponse = requests.post('https://exp.host/--/api/v2/push/send', headers = reqheaders, data = json.dumps(pushArr))
 
     if postResponse.status_code == 200:
-        print('push notifications sent successfully')
+        print('Push notifications sent successfully')
     else:
         print('something went wrong when sending push notifications')
     
+    handleErrors(postResponse.json(), pushTokens)
+
 def main():
-    print('Enter the id of the event you are promoting:', end = ' ')
+    print('Enter the event id or the share link:', end = ' ')
     eventId = input()
+
+    if 'https://' in eventId and ('uevent.app' in eventId or 'eventhubum.com' in eventId) and 'eventId=' in eventId:
+        eventId = str(int(eventId[eventId.rindex('eventId=') + 8:]) + 10351)
+    
     eventjson = getEvent(eventId)
     
     if not eventjson is None:
-        print(eventjson)
-        tokenArray = getPushTokens()
-        sendNotis(tokenArray, eventjson)
+        print()
+        for key in eventjson:
+            print(key + ': ' + str(eventjson[key]))
+        print()
+        print('Confirm this is the right event (Y/N):', end = ' ')
+        confirm = input()
+
+        if confirm == 'Y' or confirm == 'y':
+            print()
+            tokenArray = getPushTokens()
+            sendNotis(tokenArray, eventjson)
     else:
         print('Event Request Failed')
 
